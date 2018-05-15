61fafc121cc913c10fd504b0dcf34bc300dc2ccc
Francois Romain <francoisromain@gmail.com>
2018-05-15 19:13:29 +0200

    chroe: improve logging in import script

--- data/scripts/_array.js
+++ data/scripts/_array.js
@@ -1,12 +1,32 @@
 require('../../mongoose/index')
+const chalk = require('chalk')
 
 module.exports = name => {
   const Model = require(`../../mongoose/models/${name}`)
-
   const elements = require(`../sources/${name}.json`)
+  const log = (current, total, e, color) => {
+    console.log(
+      chalk.bold[color](current) + ' / ' + total + `. import ${e} in ${name}.`
+    )
+    if (current === total) {
+      console.log(chalk.bold.green(`Processed ${total} elements.`))
+    }
+  }
+
+  let n = 0
 
   elements.forEach(element => {
     const document = new Model(element)
-    return document.save().then(sub => sub, err => err)
+
+    document.save().then(
+      el => {
+        n++
+        log(n, elements.length, el, 'green')
+      },
+      err => {
+        n++
+        log(n, elements.length, err, 'red')
+      }
+    )
   })
 }
