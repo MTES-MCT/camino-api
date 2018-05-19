0c216f03ca9156669f8607fcb5dc182534f3e6ea
Francois Romain <francoisromain@gmail.com>
2018-05-19 18:38:12 +0200

    chore: script to convert graphql DSL to query result

---
+++ _tools/graphql-query-result.js
@@ -0,0 +1,17 @@
+const fetch = require('node-fetch')
+const { introspectionQuery } = require('graphql')
+const fs = require('fs')
+const url = `http://localhost:4000`
+
+fetch(url, {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({ query: introspectionQuery })
+})
+  .then(res => res.json())
+  .then(res =>
+    fs.writeFileSync(
+      '_tools/graphql-query-result.json',
+      JSON.stringify(res.data, null, 2)
+    )
+  )
