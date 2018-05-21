a1e65fc960e32e3cccc4906ca6636e7d6ae462bd
Francois Romain <francoisromain@gmail.com>
2018-05-21 11:30:52 +0200

    fix: delay seeding of join tables

--- _tools/knex/seeds/titres-structure.js
+++ _tools/knex/seeds/titres-structure.js
@@ -4,19 +4,22 @@ const types = require('../../sources/titres-types.json')
 const typesPhases = require('../../sources/titres-types-phases.json')
 
 exports.seed = (knex, Promise) => {
-  // Deletes ALL existing entries
   return Promise.all([
     knex('titres_domaines_types').del(),
     knex('titres_domaines').del(),
     knex('titres_types').del(),
     knex('titres_types_phases').del()
-  ]).then(() => {
-    // Inserts seed entries
-    return Promise.all([
-      knex('titres_domaines').insert(domaines),
-      knex('titres_types').insert(types),
-      knex('titres_domaines_types').insert(domainesTypes),
-      knex('titres_types_phases').insert(typesPhases)
-    ])
-  })
+  ])
+    .then(() => {
+      return Promise.all([
+        knex('titres_domaines').insert(domaines),
+        knex('titres_types').insert(types)
+      ])
+    })
+    .then(() => {
+      return Promise.all([
+        knex('titres_types_phases').insert(typesPhases),
+        knex('titres_domaines_types').insert(domainesTypes)
+      ])
+    })
 }
