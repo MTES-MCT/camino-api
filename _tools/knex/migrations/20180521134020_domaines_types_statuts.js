52439d2c190ff39109667f34c35fefc471808db8
Francois Romain <francoisromain@gmail.com>
2018-07-16 20:20:45 +0200

    feat: supprime domaine/couleur

--- _tools/knex/migrations/20180521134020_domaines_types_statuts.js
+++ _tools/knex/migrations/20180521134020_domaines_types_statuts.js
@@ -3,7 +3,6 @@ exports.up = knex => {
     .createTable('domaines', table => {
       table.string('id', 1).primary()
       table.string('nom').notNullable()
-      table.string('couleur')
     })
     .createTable('types', table => {
       table.string('id', 3).primary()
