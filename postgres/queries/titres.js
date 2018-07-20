01a749be443632d2e63289790ab09a16ac3cc7da
Francois Romain <francoisromain@gmail.com>
2018-07-20 18:55:52 +0200

    feat: filtre les titres par noms

--- postgres/queries/titres.js
+++ postgres/queries/titres.js
@@ -1,3 +1,4 @@
+const { raw } = require('objection')
 const Titres = require('../models/titres')
 const { hasPermission } = require('../../auth/permissions')
 const titresOptions = require('./_titres-options')
@@ -9,36 +10,32 @@ const queries = {
       .findById(id)
       .eager(titresOptions.eager),
 
-  titres: async ({ typeIds, domaineIds, statutIds, substances }, user) =>
-    // Titres.raw(
-    //   `SELECT t.* FROM titres t
-    // WHERE t.typeId in (:typeIds)
-    // AND t.domaineId in (:domaineIds)
-    // AND t.statutId in (:statutIds)
-    // AND t.police in (:polices)
-    // AND (
-    //     lower(s.label) in (:substances)
-    //     OR lower(s.symbole) IN (:substances)
-    // )
-    // LEFT JOIN titresSubstancesPrincipales tsp on tsp.titreId = t.id
-    // LEFT JOIN substances s on s.id = tsp.substanceId    `,
-    //   {
-    //     typeIds,
-    //     domaineIds,
-    //     statutIds,
-    //     polices,
-    //     substances
-    //   }
-    // ),
-    Titres.query()
+  titres: async (
+    { typeIds, domaineIds, statutIds, substances, noms },
+    user
+  ) => {
+    const q = Titres.query()
       .skipUndefined()
+      .eager(titresOptions.eager)
       .whereIn('titres.typeId', typeIds)
       .whereIn('titres.domaineId', domaineIds)
       .whereIn('titres.statutId', statutIds)
-      .joinRelation('demarches.etapes.substances')
-      .whereIn('demarches:etapes:substances.id', substances)
-      .orWhereIn('demarches:etapes:substances.nom', substances)
-      .eager(titresOptions.eager),
+
+    if (noms) {
+      q.whereRaw(`lower(??) ~* ${noms.map(n => '?').join('|')}`, [
+        'titres.nom',
+        ...noms.map(n => n.toLowerCase())
+      ])
+    }
+
+    if (substances) {
+      q.joinRelation('demarches.etapes.substances')
+        .whereIn('demarches:etapes:substances.id', substances)
+        .orWhereIn('demarches:etapes:substances.nom', substances)
+    }
+
+    return q
+  },
 
   titreAjouter: async (titre, user) =>
     hasPermission('admin', user)
