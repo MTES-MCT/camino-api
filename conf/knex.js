418d655e09a1a7de2bc7802c83dd8a9043590809
Francois Romain <francoisromain@gmail.com>
2018-07-25 16:22:05 +0200

    fix: ajout d'une variable d'environement PGPASSWD

--- conf/knex.js
+++ conf/knex.js
@@ -5,7 +5,8 @@ const connection = {
   host: process.env.PGHOST,
   port: process.env.PGPORT,
   database: process.env.PGDATABASE,
-  user: process.env.PGUSER
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD
 }
 
 const knexConfig = {
