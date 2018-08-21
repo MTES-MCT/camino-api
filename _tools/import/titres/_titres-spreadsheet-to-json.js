ade5621ec493a78c1efc0f9ec6b054d7ca9c42e3
Francois Romain <francoisromain@gmail.com>
2018-08-21 10:27:36 +0200

    feat: parse les donnée json lors de l'import depuis les spreadsheets

--- _tools/import/titres/_titres-spreadsheet-to-json.js
+++ _tools/import/titres/_titres-spreadsheet-to-json.js
@@ -1,18 +1,18 @@
 const spreadsheetToJson = require('../_utils/_spreadsheet-to-json')
 const filePathCreate = require('../_utils/file-path-create')
 
-const titresCb = json =>
+const jsonParse = value => json =>
   json.map(j =>
     Object.keys(j).reduce((res, cur) => {
-      res[cur] = cur === 'references' ? JSON.parse(j[cur]) : j[cur]
+      res[cur] = cur === value ? JSON.parse(j[cur]) : j[cur]
       return res
     }, {})
   )
 
 const tables = [
-  { name: '', cb: titresCb },
+  { name: '', cb: jsonParse('references') },
   { name: '_demarches', cb: null },
-  { name: '_etapes', cb: etapesCb },
+  { name: '_etapes', cb: jsonParse('visas') },
   { name: '_points', cb: null },
   { name: '_points_references', cb: null },
   { name: '_documents', cb: null },
