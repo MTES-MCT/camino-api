8351bdbbb0a2b9966546f0e05543f9f846765b3b
Francois Romain <francoisromain@gmail.com>
2018-08-30 19:33:49 +0200

    refactor: renomme titre-a en titres-r

--- _tools/knex/seeds/3-repertoire.js
+++ _tools/knex/seeds/3-repertoire.js
@@ -1,22 +1,22 @@
-const entreprisesA = require('../../sources/repertoire-entreprises-a.json')
 const entreprisesC = require('../../sources/repertoire-entreprises-c.json')
 const entreprisesF = require('../../sources/repertoire-entreprises-f.json')
 const entreprisesG = require('../../sources/repertoire-entreprises-g.json')
 const entreprisesH = require('../../sources/repertoire-entreprises-h.json')
 const entreprisesM = require('../../sources/repertoire-entreprises-m.json')
 const entreprisesM973 = require('../../sources/repertoire-entreprises-m973.json')
+const entreprisesR = require('../../sources/repertoire-entreprises-r.json')
 const entreprisesS = require('../../sources/repertoire-entreprises-s.json')
 const entreprisesW = require('../../sources/repertoire-entreprises-w.json')
 const administrations = require('../../sources/repertoire-administrations.json')
 
 const entreprises = [
-  ...entreprisesA,
   ...entreprisesC,
   ...entreprisesF,
   ...entreprisesG,
   ...entreprisesH,
   ...entreprisesM,
   ...entreprisesM973,
+  ...entreprisesR,
   ...entreprisesS,
   ...entreprisesW
 ]
