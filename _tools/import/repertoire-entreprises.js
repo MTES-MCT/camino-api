d1a9bc0d1b8e42d6eb3f620467a4df3664cd0e3c
Francois Romain <francoisromain@gmail.com>
2018-08-30 19:33:49 +0200

    refactor: renomme titre-a en titres-r

--- _tools/import/repertoire-entreprises.js
+++ _tools/import/repertoire-entreprises.js
@@ -2,7 +2,7 @@ const spreadsheetToJson = require('./_utils/_spreadsheet-to-json')
 const spreadsheetId = '1DbpD49cEHkbAAh-roSWGRr4GeMJVe9c_ymSNaBOJVkc'
 const filePathCreate = require('./_utils/file-path-create')
 
-const tables = ['a', 'c', 'f', 'g', 'h', 'm', 'm973', 's', 'w']
+const tables = ['c', 'f', 'g', 'h', 'm', 'm973', 'r', 's', 'w']
 
 module.exports = async () =>
   Promise.all([
--- _tools/import/titres/r.js
+++ _tools/import/titres/r.js
@@ -1,5 +1,5 @@
 const titresSpreadsheetToJson = require('./_titres-spreadsheet-to-json')
 const spreadsheetId = '1dfzKTiUO3xAPm7IsI-Ud5PcVZDvTxFODSzoePu_aFQg'
-const type = 'a'
+const type = 'r'
 
 module.exports = async () => titresSpreadsheetToJson(spreadsheetId, type)
