ddec9e30f1aaa0718b5aacb05bdfed394c80b6e5
Francois Romain <francoisromain@gmail.com>
2018-09-05 03:15:03 +0200

    feat: mise à jour de l'ordre des démarches

---
+++ tasks/_utils/titre-statut-id-find.js
@@ -0,0 +1,44 @@
+const titreStatutIdFind = td => {
+  let titreStatut
+
+  if (
+    // si
+  // - la date du jour est inférieure à la date d’échéance
+  // - aucune démarche est en instruction || déposée || initiée
+
+  ) {
+    // alors
+    // - le statut du titre est valide
+  } else if (
+// sinon si
+  // - une démarche a le statut en instruction
+
+  ) {
+    // alors
+    // - le statut du titre est modification en instance
+  } else if (
+// sinon si
+  // - la seule démarche est une démarche d’octroi avec le statut en instruction || déposée
+
+
+  ) {
+    // alors
+    // - le statut du titre est demande initiale
+  } else if (
+// sinon si
+  // - la seule démarche est une démarche d’octroi 
+  // - le statut rejetée || classée sans suite || retirée
+
+  ) {
+    // alors
+    // - le statut du titre est demande classée
+  } else {
+    //  sinon --> 3. base case
+    //  - le statut du titre est indéterminé
+    titreStatut = 'ind'
+  }
+
+  return titreStatut
+}
+
+module.exports = titreStatutIdFind
