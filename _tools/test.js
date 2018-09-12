34708dc5e8cb3dd28c87fd5de519740e1b51a189
Francois Romain <francoisromain@gmail.com>
2018-09-12 15:02:35 +0200

    fix: mise à jour du script de test

--- _tools/test.js
+++ _tools/test.js
@@ -1,4 +1,4 @@
-const entreprisesA = require('./sources/repertoire-entreprises-a.json')
+const entreprisesR = require('./sources/repertoire-entreprises-r.json')
 const entreprisesC = require('./sources/repertoire-entreprises-c.json')
 const entreprisesF = require('./sources/repertoire-entreprises-f.json')
 const entreprisesG = require('./sources/repertoire-entreprises-g.json')
@@ -9,18 +9,38 @@ const entreprisesS = require('./sources/repertoire-entreprises-s.json')
 const entreprisesW = require('./sources/repertoire-entreprises-w.json')
 
 const entreprises = [
-  ...entreprisesA,
   ...entreprisesC,
   ...entreprisesF,
   ...entreprisesG,
   ...entreprisesH,
   ...entreprisesM,
   ...entreprisesM973,
+  ...entreprisesR,
   ...entreprisesS,
   ...entreprisesW
 ]
 
-const titulaires = require('./sources/titres-h-titulaires.json')
+const titulairesR = require('./sources/titres-r-titulaires.json')
+const titulairesC = require('./sources/titres-c-titulaires.json')
+const titulairesF = require('./sources/titres-f-titulaires.json')
+const titulairesG = require('./sources/titres-g-titulaires.json')
+const titulairesH = require('./sources/titres-h-titulaires.json')
+const titulairesM = require('./sources/titres-m-titulaires.json')
+const titulairesM973 = require('./sources/titres-m973-titulaires.json')
+const titulairesS = require('./sources/titres-s-titulaires.json')
+const titulairesW = require('./sources/titres-w-titulaires.json')
+
+const titulaires = [
+  ...titulairesR,
+  ...titulairesC,
+  ...titulairesF,
+  ...titulairesG,
+  ...titulairesH,
+  ...titulairesM,
+  ...titulairesM973,
+  ...titulairesS,
+  ...titulairesW
+]
 
 // demarches.forEach(d => {
 //   const ti = titres.find(t => t.id === d.titre_id)
@@ -30,6 +50,6 @@ const titulaires = require('./sources/titres-h-titulaires.json')
 titulaires.forEach(t => {
   const d = entreprises.find(e => e.id === t.entreprise_id)
   if (!d) {
-    console.log(t.titre_etape_id)
+    console.log(t.titre_etape_id, t.entreprise_id)
   }
 })
