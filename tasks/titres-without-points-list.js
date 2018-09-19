f72b5e01814a6e1ff4f36e9f6c4de928e0b98f0b
Francois Romain <francoisromain@gmail.com>
2018-09-19 07:58:34 +0200

    feat: créatuion d'un script pour lister les titres sans périmètre

---
+++ tasks/titres-without-points-list.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+require('../postgres')
+
+const { titresGet } = require('../postgres/queries/titres')
+
+const titresWithoutPointsList = async () => {
+  const titres = await titresGet({})
+
+  const list = titres.filter(
+    titre =>
+      titre.demarches.every(demarche =>
+        demarche.etapes.every(etape => etape.points.length === 0)
+      ) && titre.statutId === 'val'
+  )
+
+  return list.map(titre => titre.id)
+}
+
+const run = async () => {
+  const res = await titresWithoutPointsList()
+  console.log(res)
+  console.log('Done')
+
+  process.exit()
+}
+
+run()
