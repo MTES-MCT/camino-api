89da71426b8733d07a46d09b260ca420e09d08bd
Francois Romain <francoisromain@gmail.com>
2018-09-23 21:47:27 +0200

    feat: exporte les démarches

--- _tools/export/titres/_titres-db-to-spreadsheet.js
+++ _tools/export/titres/_titres-db-to-spreadsheet.js
@@ -6,6 +6,15 @@ const GoogleSpreadsheet = require('google-spreadsheet')
 const { titresGet } = require('../../../postgres/queries/titres')
 const credentials = require('../credentials')
 
+const {
+  gssUseServiceAccountAuth,
+  gssGetInfo,
+  worksheetRemove,
+  worksheetAdd,
+  rowAdd,
+  cellsGet
+} = require('../_utils/google-spreadsheet-promisify')
+
 const worksheets = [
   {
     title: 'titres',
@@ -41,7 +50,7 @@ module.exports = async (spreadsheetId, domaineId) => {
   const gss = new GoogleSpreadsheet(spreadsheetId)
 
   // authentification dans google
-  await gssUseServiceAccountAuth(gss)
+  await gssUseServiceAccountAuth(gss, credentials)
 
   // obtiens les infos sur la spreadsheet
   const infos = await gssGetInfo(gss)
@@ -52,18 +61,18 @@ module.exports = async (spreadsheetId, domaineId) => {
   }
 
   // retourne un tableau des onglets à supprimer
-  const worksheetsToRemove = infos.worksheets.filter(w => w.title !== 'tmp')
+  const worksheetsRemove = infos.worksheets.filter(w => w.title !== 'tmp')
 
   // retourne un tableau avec les requêtes pour supprimer les onglets
-  const worksheetToRemovePromises = worksheetsToRemove.map(w =>
+  const worksheetRemovePromises = worksheetsRemove.map(w =>
     worksheetRemove(gss, w)
   )
 
   // supprime les onglets
-  await Promise.all(worksheetToRemovePromises)
+  await Promise.all(worksheetRemovePromises)
 
   // retourne un tableau avec les requêtes pour ajouter les nouveaux onglets
-  const worksheetsToAddPromises = worksheets.map(({ title, headers }) => () =>
+  const worksheetsAddPromises = worksheets.map(({ title, headers }) => () =>
     worksheetAdd(gss, {
       title: decamelize(title),
       headers: headers.map(h => (h === 'id' ? 'Id' : h)),
@@ -75,24 +84,25 @@ module.exports = async (spreadsheetId, domaineId) => {
   // on utilise une queue plutôt que Promise.all
   // pour que les onglets soient créés dans l'ordre
   const worksheetQueue = new PQueue({ concurrency: 1 })
-  const worksheetsNew = await worksheetQueue.addAll(worksheetsToAddPromises)
+  const worksheetsNew = await worksheetQueue.addAll(worksheetsAddPromises)
 
-  // renseigne l'id des worksheets créés dans le tableau original
-
+  // renseigne l'id des worksheets créées
   worksheets.forEach(w => {
-    const worksheetNew = worksheetsNew.find(wn => wn.title === w.title)
+    const worksheetNew = worksheetsNew.find(
+      wn => wn.title === decamelize(w.title)
+    )
     w.id = worksheetNew && worksheetNew.id
   })
 
   // est ce que l'onglet tmp existe ?
-  const worksheetTmpToRemove = infos.worksheets.find(w => w.title === 'tmp')
+  const worksheetTmpRemove = infos.worksheets.find(w => w.title === 'tmp')
 
   // si l'onglet 'tmp' existe, on le supprime
-  if (worksheetTmpToRemove) {
-    await worksheetRemove(gss, worksheetTmpToRemove)
+  if (worksheetTmpRemove) {
+    await worksheetRemove(gss, worksheetTmpRemove)
   }
 
-  const titresRowsToAddPromises = titres.map(titre => {
+  const titresRowsAddPromises = titres.map(titre => {
     const worksheet = worksheets.find(w => w.title === 'titres')
     const row = worksheet.headers.reduce(
       (r, header) =>
@@ -111,98 +121,48 @@ module.exports = async (spreadsheetId, domaineId) => {
   })
 
   // on utilise une queue plutôt que Promise.all
-  // pour que les onglets soient créés dans l'ordre
+  // pour éviter une erreur de saturation de google api
   const titreRowsQueue = new PQueue({ concurrency: 20 })
-  await titreRowsQueue.addAll(titresRowsToAddPromises)
+  await titreRowsQueue.addAll(titresRowsAddPromises)
 
-  const worksheet = worksheets.find(w => w.title === 'titres')
-  console.log(worksheet)
-  const cellId = await cellsGet(gss, worksheet.id, {
-    'min-row': 1,
-    'max-row': 1,
-    'min-col': 1,
-    'max-col': 1
-  })
+  const titresDemarchesRowsAddPromises = titres.reduce((res, titre) => {
+    const worksheet = worksheets.find(w => w.title === 'titresDemarches')
+    const titreDemarchesRowsAddPromises = titre['demarches'].map(d => {
+      const row = worksheet.headers.reduce(
+        (r, header) =>
+          Object.assign(r, {
+            [header === 'id' ? 'Id' : decamelize(header)]: d[header]
+          }),
+        {}
+      )
+      return () => rowAdd(gss, worksheet.id, row)
+    })
+    return [...res, ...titreDemarchesRowsAddPromises]
+  }, [])
 
-  cellId[0].setValue('id', (err, res) => {
-    if (err) {
-      console.log(err)
-    } else {
-      console.log(res)
-    }
+  // on utilise une queue plutôt que Promise.all
+  // pour éviter une erreur de saturation de google api
+  const titresDemarchesRowsQueue = new PQueue({ concurrency: 20 })
+  await titresDemarchesRowsQueue.addAll(titresDemarchesRowsAddPromises)
+
+  const worksheetsWithId = worksheets.filter(w =>
+    w.headers.find(h => h === 'id')
+  )
+
+  worksheetsWithId.forEach(async worksheet => {
+    const cellIds = await cellsGet(gss, worksheet.id, {
+      'min-row': 1,
+      'max-row': 1,
+      'min-col': 1,
+      'max-col': 1
+    })
+
+    cellIds[0].setValue('id', (err, res) => {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log(res)
+      }
+    })
   })
 }
-
-const gssUseServiceAccountAuth = gss =>
-  new Promise((resolve, reject) =>
-    gss.useServiceAccountAuth(credentials, (err, res) => {
-      if (err) {
-        console.log(err)
-        reject(err)
-      } else {
-        console.log('authentification: succès')
-        resolve(res)
-      }
-    })
-  )
-
-const gssGetInfo = gss =>
-  new Promise((resolve, reject) =>
-    gss.getInfo((err, res) => {
-      if (err) {
-        console.log(err)
-        reject(err)
-      } else {
-        console.log('spreadsheet:', res.title)
-        resolve(res)
-      }
-    })
-  )
-
-const worksheetRemove = (gss, worksheet) =>
-  new Promise((resolve, reject) => {
-    gss.removeWorksheet(worksheet.id, (err, res) => {
-      if (err) {
-        console.log('nop ', err)
-        reject(err)
-      } else {
-        console.log(`worksheet supprimée: ${worksheet.title}`)
-        resolve(res)
-      }
-    })
-  })
-
-const worksheetAdd = (gss, worksheet) =>
-  new Promise((resolve, reject) =>
-    gss.addWorksheet(worksheet, (err, res) => {
-      if (err) {
-        reject(err)
-      } else {
-        console.log(`worksheet ajoutée: ${worksheet.title}`)
-        resolve(res)
-      }
-    })
-  )
-
-const rowAdd = (gss, worksheetId, row) =>
-  new Promise((resolve, reject) =>
-    gss.addRow(worksheetId, row, (err, res) => {
-      if (err) {
-        reject(err)
-      } else {
-        console.log(`row ajoutée: ${row.Id}`)
-        resolve(res)
-      }
-    })
-  )
-
-const cellsGet = (gss, worksheetId, options) =>
-  new Promise((resolve, reject) =>
-    gss.getCells(worksheetId, options, (err, res) => {
-      if (err) {
-        reject(err)
-      } else {
-        resolve(res)
-      }
-    })
-  )
