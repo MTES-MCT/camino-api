a43468617267d70efff3dddf120e11562486197e
Francois Romain <francoisromain@gmail.com>
2018-09-27 08:33:34 +0200

    feat: ajoute l'authentification d'un utilisateur

--- postgres/queries/titres-phases.js
+++ postgres/queries/titres-phases.js
@@ -1,35 +1,37 @@
 const TitresPhases = require('../models/titres-phases')
 const options = require('./_options')
 
-const queries = {
-  titresPhasesGet: async ({ titreDemarcheIds }, user) =>
-    TitresPhases.query()
-      .skipUndefined()
-      .eager(options.phases.eager)
-      .whereIn('titresPhases.titreDemarcheId', titreDemarcheIds),
+const titresPhasesGet = async ({ titreDemarcheIds }, user) =>
+  TitresPhases.query()
+    .skipUndefined()
+    .eager(options.phases.eager)
+    .whereIn('titresPhases.titreDemarcheId', titreDemarcheIds)
 
-  titrePhaseUpdate: async ({ titrePhase }, user) =>
-    TitresPhases.query().upsertGraph(titrePhase, { insertMissing: true }),
-  // {
-  //   console.log(titrePhase)
-  //   const exists = await TitresPhases.query().where(
-  //     'titreDemarcheId',
-  //     titrePhase.titreDemarcheId
-  //   )
+const titrePhaseUpdate = async ({ titrePhase }, user) =>
+  TitresPhases.query().upsertGraph(titrePhase, { insertMissing: true })
+// {
+//   console.log(titrePhase)
+//   const exists = await TitresPhases.query().where(
+//     'titreDemarcheId',
+//     titrePhase.titreDemarcheId
+//   )
 
-  //   if (exists) {
-  //     return TitresPhases.query()
-  //       .where('titreDemarcheId', titrePhase.titreDemarcheId)
-  //       .patch(titrePhase)
-  //   } else {
-  //     return TitresPhases.query().insert(titrePhase)
-  //   }
-  // },
+//   if (exists) {
+//     return TitresPhases.query()
+//       .where('titreDemarcheId', titrePhase.titreDemarcheId)
+//       .patch(titrePhase)
+//   } else {
+//     return TitresPhases.query().insert(titrePhase)
+//   }
+// },
 
-  titrePhaseDelete: async ({ titreDemarcheId }, user) =>
-    TitresPhases.query()
-      .deleteById(titreDemarcheId)
-      .returning('*')
+const titrePhaseDelete = async ({ titreDemarcheId }, user) =>
+  TitresPhases.query()
+    .deleteById(titreDemarcheId)
+    .returning('*')
+
+module.exports = {
+  titresPhasesGet,
+  titrePhaseUpdate,
+  titrePhaseDelete
 }
-
-module.exports = queries
