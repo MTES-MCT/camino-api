3434fcc37534726164479f0ec804cf2dcc177162
Francois Romain <francoisromain@gmail.com>
2018-09-27 16:12:47 +0200

    refactor: simplifie la config knex

---
+++ _tools/knex/config.js
@@ -0,0 +1,26 @@
+require('dotenv').config({ path: '../../.env' })
+const { knexSnakeCaseMappers } = require('objection')
+
+const connection = {
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  database: process.env.PGDATABASE,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD
+}
+
+console.log(connection)
+
+const knexConfig = {
+  client: 'pg',
+  connection,
+  migrations: {
+    directory: './migrations'
+  },
+  seeds: {
+    directory: './seeds'
+  },
+  ...knexSnakeCaseMappers()
+}
+
+module.exports = knexConfig
