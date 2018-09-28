9ddbd780af7790618f1eca346938e729505f5046
Francois Romain <francoisromain@gmail.com>
2018-09-28 21:31:05 +0200

    feat: supprime le lien entre titres et utilisateurs

--- postgres/models/utilisateurs.js
+++ postgres/models/utilisateurs.js
@@ -1,4 +1,5 @@
 const { Model } = require('objection')
+const Groupes = require('./groupes')
 
 class Utilisateurs extends Model {
   static get tableName() {
@@ -13,13 +14,30 @@ class Utilisateurs extends Model {
       properties: {
         id: { type: 'string', maxLength: 64 },
         email: { type: 'string' },
-        password: { type: 'string' },
+        motDePasse: { type: 'string' },
         nom: { type: 'string' },
         prenom: { type: 'string' },
         telephone_fixe: { type: 'string' },
         telephone_mobile: { type: 'string' },
         entrepriseId: { type: 'string', maxLength: 64 },
         administrationId: { type: 'string', maxLength: 64 }
+      }
+    }
+  }
+
+  static get relationMappings() {
+    return {
+      groupes: {
+        relation: Model.ManyToManyRelation,
+        modelClass: Groupes,
+        join: {
+          from: 'utilisateurs.id',
+          through: {
+            from: 'utilisateursGroupes.utilisateurId',
+            to: 'utilisateursGroupes.groupeId'
+          },
+          to: 'groupes.id'
+        }
       }
     }
   }
