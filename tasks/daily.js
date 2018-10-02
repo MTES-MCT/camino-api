042668b08ba681bd0ddf002612ff907ebe6c3f10
Francois Romain <francoisromain@gmail.com>
2018-10-02 23:11:43 +0200

    refactor: optimise les calcule de l'ordre des démarches

--- tasks/daily.js
+++ tasks/daily.js
@@ -8,10 +8,24 @@ const titresStatutIdsUpdate = require('./titres-statut-ids-update')
 const titresPhasesUpdate = require('./titres-phases-update')
 
 const run = async () => {
+  // détermine l'ordre des étapes
+  // en fonction de leur date
   const a = await titresEtapesOrdreUpdate()
+
+  // détermine le statut des démarches
+  // en fonction de ses étapes (type, ordre, statut)
   const b = await titresDemarchesStatutIdUpdate()
+
+  // détermine l'ordre des démarche
+  // en fonction de la date de leur première étape
   const c = await titresDemarchesOrdreUpdate()
+
+  // détermine le statut des titres
+  // en fonction des démarches et de la date du jour
   const d = await titresStatutIdsUpdate()
+
+  // détermine les phases
+  // en fonction des démarches et de la date du jour
   const e = await titresPhasesUpdate()
 
   console.log(a)
