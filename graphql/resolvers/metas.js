2b8a72e7518a0181b6ca5880a3eb1000a63b0c0e
Francois Romain <francoisromain@gmail.com>
2018-10-04 15:47:06 +0200

    feat: expose les entreprises via l'API

--- graphql/resolvers/metas.js
+++ graphql/resolvers/metas.js
@@ -1,9 +1,6 @@
 const { types, domaines, statuts } = require('../../postgres/queries/metas')
 
 const resolvers = {
-  types: async (variables, context, info) => types(),
-  domaines: async (variables, context, info) => domaines(),
-  statuts: async (variables, context, info) => statuts(),
   metas: async (variables, context, info) => {
     const t = await types()
     const d = await domaines()
