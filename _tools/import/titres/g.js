d614a940ebb85ed9f640ab8bedc9a8e2265d9011
Francois Romain <francoisromain@gmail.com>
2018-10-04 23:39:54 +0200

    feat: change l'id de la spreadsheet camino-db-titres-g

--- _tools/import/titres/g.js
+++ _tools/import/titres/g.js
@@ -1,5 +1,5 @@
 const titresSpreadsheetToJson = require('./_titres-spreadsheet-to-json')
-const spreadsheetId = '1zIwhE7UkCMtfo3zqDpU09swk2kmKrzslg8r4oQmAbz4'
+const spreadsheetId = '1-jwwDe7UZfiluHEDB57sTo8ANoj4J3W9oH81B-Hb17c'
 const type = 'g'
 
 module.exports = async () => titresSpreadsheetToJson(spreadsheetId, type)
