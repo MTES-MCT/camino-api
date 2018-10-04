92e321d46ca5aa02ba3cc2f6fcad41948bc6224d
Francois Romain <francoisromain@gmail.com>
2018-10-04 13:47:02 +0200

    fix: recherche sur les substances

--- postgres/queries/titres.js
+++ postgres/queries/titres.js
@@ -30,19 +30,43 @@ const titresGet = async ({
   if (substances) {
     q.where(builder => {
       builder
-        .whereIn(
-          'demarches:etapes:substances.id',
-          substances.map(n => n.toLowerCase())
-        )
-        .orWhereIn(
-          'demarches:etapes:substances.nom',
-          substances.map(n => n.toLowerCase())
-        )
-        .orWhereIn(
-          'demarches:etapes:substances.symbole',
-          substances.map(n => n.toLowerCase())
-        )
-    }).joinRelation('demarches.etapes.substances')
+        .whereIn('substances:legales.id', substances.map(n => n.toLowerCase()))
+        .orWhereRaw(`lower(??) ~* ${substances.map(n => '?').join('|')}`, [
+          'substances:legales.nom',
+          ...substances.map(n => n.toLowerCase())
+        ])
+        .orWhereRaw(`lower(??) ~* ${substances.map(n => '?').join('|')}`, [
+          'substances.nom',
+          ...substances.map(n => n.toLowerCase())
+        ])
+        .orWhereRaw(`lower(??) ~* ${substances.map(n => '?').join('|')}`, [
+          'substances.symbole',
+          ...substances.map(n => n.toLowerCase())
+        ])
+    }).joinRelation('substances.legales')
+
+    // recherche dans l'historique des étapes
+    // bug: retourne plusieurs fois le même titre
+    // si la substance se trouve dans plusieurs étapes
+    // -> solution (to-do): adjusting the result set of the first query for parents.
+    // check out the whereExists() / relatedQuery() example here
+    // http://vincit.github.io/objection.js/#relatedquery
+
+    // q.where(builder => {
+    //   builder
+    //     .whereIn(
+    //       'demarches:etapes:substances.id',
+    //       substances.map(n => n.toLowerCase())
+    //     )
+    //     .orWhereIn(
+    //       'demarches:etapes:substances.nom',
+    //       substances.map(n => n.toLowerCase())
+    //     )
+    //     .orWhereIn(
+    //       'demarches:etapes:substances.symbole',
+    //       substances.map(n => n.toLowerCase())
+    //     )
+    // }).joinRelation('demarches.etapes.substances')
   }
 
   return q
