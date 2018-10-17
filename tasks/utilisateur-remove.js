b798b7c7360cf9a8e6bfb0cff35e9548ea251400
Francois Romain <francoisromain@gmail.com>
2018-10-17 19:20:35 +0200

    fix:

--- tasks/utilisateur-remove.js
+++ tasks/utilisateur-remove.js
@@ -2,15 +2,10 @@ require('dotenv').config()
 require('../postgres')
 const { utilisateurRemove } = require('../postgres/queries/utilisateurs')
 
-const group = {
-  id: 'admin',
-  role: 'admin'
-}
-
 const id = process.env.ADMIN_ID
 
 const run = async () => {
-  await utilisateurRemove(id, group)
+  await utilisateurRemove(id)
   console.log('Utilisateur supprimé')
   process.exit()
 }
