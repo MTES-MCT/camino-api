56b3900afe6ec49905804454da294b0b9b3bcb63
Francois Romain <francoisromain@gmail.com>
2018-10-17 16:49:55 +0200

    refactor:  dynamise les imports de fichiers

--- _tools/knex/seeds/5-titres.js
+++ _tools/knex/seeds/5-titres.js
@@ -1,232 +1,29 @@
-const c = require('../../sources/titres-c.json')
-const cDemarches = require('../../sources/titres-c-demarches.json')
-const cEtapes = require('../../sources/titres-c-etapes.json')
-const cEmprises = require('../../sources/titres-c-emprises.json')
-const cPoints = require('../../sources/titres-c-points.json')
-const cPointsReferences = require('../../sources/titres-c-points-references.json')
-const cDocuments = require('../../sources/titres-c-documents.json')
-const cSubstances = require('../../sources/titres-c-substances.json')
-const cTitulaires = require('../../sources/titres-c-titulaires.json')
-const cAmodiataires = require('../../sources/titres-c-amodiataires.json')
-const cErreurs = require('../../sources/titres-c-erreurs.json')
-
-const f = require('../../sources/titres-f.json')
-const fDemarches = require('../../sources/titres-f-demarches.json')
-const fEtapes = require('../../sources/titres-f-etapes.json')
-const fEmprises = require('../../sources/titres-f-emprises.json')
-const fPoints = require('../../sources/titres-f-points.json')
-const fPointsReferences = require('../../sources/titres-f-points-references.json')
-const fDocuments = require('../../sources/titres-f-documents.json')
-const fSubstances = require('../../sources/titres-f-substances.json')
-const fTitulaires = require('../../sources/titres-f-titulaires.json')
-const fAmodiataires = require('../../sources/titres-f-amodiataires.json')
-const fErreurs = require('../../sources/titres-f-erreurs.json')
-
-const g = require('../../sources/titres-g.json')
-const gDemarches = require('../../sources/titres-g-demarches.json')
-const gEtapes = require('../../sources/titres-g-etapes.json')
-const gEmprises = require('../../sources/titres-g-emprises.json')
-const gPoints = require('../../sources/titres-g-points.json')
-const gPointsReferences = require('../../sources/titres-g-points-references.json')
-const gDocuments = require('../../sources/titres-g-documents.json')
-const gSubstances = require('../../sources/titres-g-substances.json')
-const gTitulaires = require('../../sources/titres-g-titulaires.json')
-const gAmodiataires = require('../../sources/titres-g-amodiataires.json')
-const gErreurs = require('../../sources/titres-g-erreurs.json')
-
-const h = require('../../sources/titres-h.json')
-const hDemarches = require('../../sources/titres-h-demarches.json')
-const hEtapes = require('../../sources/titres-h-etapes.json')
-const hEmprises = require('../../sources/titres-h-emprises.json')
-const hPoints = require('../../sources/titres-h-points.json')
-const hPointsReferences = require('../../sources/titres-h-points-references.json')
-const hDocuments = require('../../sources/titres-h-documents.json')
-const hSubstances = require('../../sources/titres-h-substances.json')
-const hTitulaires = require('../../sources/titres-h-titulaires.json')
-const hAmodiataires = require('../../sources/titres-h-amodiataires.json')
-const hErreurs = require('../../sources/titres-h-erreurs.json')
-
-const m = require('../../sources/titres-m.json')
-const mDemarches = require('../../sources/titres-m-demarches.json')
-const mEtapes = require('../../sources/titres-m-etapes.json')
-const mEmprises = require('../../sources/titres-m-emprises.json')
-const mPoints = require('../../sources/titres-m-points.json')
-const mPointsReferences = require('../../sources/titres-m-points-references.json')
-const mDocuments = require('../../sources/titres-m-documents.json')
-const mSubstances = require('../../sources/titres-m-substances.json')
-const mTitulaires = require('../../sources/titres-m-titulaires.json')
-const mAmodiataires = require('../../sources/titres-m-amodiataires.json')
-const mErreurs = require('../../sources/titres-m-erreurs.json')
-
-const m973 = require('../../sources/titres-m973.json')
-const m973Demarches = require('../../sources/titres-m973-demarches.json')
-const m973Etapes = require('../../sources/titres-m973-etapes.json')
-const m973Emprises = require('../../sources/titres-m973-emprises.json')
-const m973Points = require('../../sources/titres-m973-points.json')
-const m973PointsReferences = require('../../sources/titres-m973-points-references.json')
-const m973Documents = require('../../sources/titres-m973-documents.json')
-const m973Substances = require('../../sources/titres-m973-substances.json')
-const m973Titulaires = require('../../sources/titres-m973-titulaires.json')
-const m973Amodiataires = require('../../sources/titres-m973-amodiataires.json')
-const m973Erreurs = require('../../sources/titres-m973-erreurs.json')
-
-const r = require('../../sources/titres-r.json')
-const rDemarches = require('../../sources/titres-r-demarches.json')
-const rEtapes = require('../../sources/titres-r-etapes.json')
-const rEmprises = require('../../sources/titres-r-emprises.json')
-const rPoints = require('../../sources/titres-r-points.json')
-const rPointsReferences = require('../../sources/titres-r-points-references.json')
-const rDocuments = require('../../sources/titres-r-documents.json')
-const rSubstances = require('../../sources/titres-r-substances.json')
-const rTitulaires = require('../../sources/titres-r-titulaires.json')
-const rAmodiataires = require('../../sources/titres-r-amodiataires.json')
-const rErreurs = require('../../sources/titres-r-erreurs.json')
-
-const s = require('../../sources/titres-s.json')
-const sDemarches = require('../../sources/titres-s-demarches.json')
-const sEtapes = require('../../sources/titres-s-etapes.json')
-const sEmprises = require('../../sources/titres-s-emprises.json')
-const sPoints = require('../../sources/titres-s-points.json')
-const sPointsReferences = require('../../sources/titres-s-points-references.json')
-const sDocuments = require('../../sources/titres-s-documents.json')
-const sSubstances = require('../../sources/titres-s-substances.json')
-const sTitulaires = require('../../sources/titres-s-titulaires.json')
-const sAmodiataires = require('../../sources/titres-s-amodiataires.json')
-const sErreurs = require('../../sources/titres-s-erreurs.json')
-
-const w = require('../../sources/titres-w.json')
-const wDemarches = require('../../sources/titres-w-demarches.json')
-const wEtapes = require('../../sources/titres-w-etapes.json')
-const wEmprises = require('../../sources/titres-w-emprises.json')
-const wPoints = require('../../sources/titres-w-points.json')
-const wPointsReferences = require('../../sources/titres-w-points-references.json')
-const wDocuments = require('../../sources/titres-w-documents.json')
-const wSubstances = require('../../sources/titres-w-substances.json')
-const wTitulaires = require('../../sources/titres-w-titulaires.json')
-const wAmodiataires = require('../../sources/titres-w-amodiataires.json')
-const wErreurs = require('../../sources/titres-w-erreurs.json')
-
-const titres = [...m, ...m973, ...h, ...s, ...c, ...g, ...r, ...w, ...f]
-
-const titresDemarches = [
-  ...mDemarches,
-  ...m973Demarches,
-  ...hDemarches,
-  ...sDemarches,
-  ...cDemarches,
-  ...gDemarches,
-  ...rDemarches,
-  ...wDemarches,
-  ...fDemarches
+const decamelize = require('decamelize')
+const types = ['c', 'f', 'g', 'h', 'm', 'm973', 'r', 's', 'w']
+const files = [
+  '',
+  'Demarches',
+  'Etapes',
+  'Emprises',
+  'Points',
+  'PointsReferences',
+  'Documents',
+  'Substances',
+  'Titulaires',
+  'Amodiataires',
+  'Erreurs'
 ]
 
-const titresEtapes = [
-  ...mEtapes,
-  ...m973Etapes,
-  ...hEtapes,
-  ...sEtapes,
-  ...cEtapes,
-  ...gEtapes,
-  ...rEtapes,
-  ...wEtapes,
-  ...fEtapes
-]
-
-const titresSubstances = [
-  ...mSubstances,
-  ...m973Substances,
-  ...hSubstances,
-  ...sSubstances,
-  ...cSubstances,
-  ...gSubstances,
-  ...rSubstances,
-  ...wSubstances,
-  ...fSubstances
-]
-
-const titresPoints = [
-  ...mPoints,
-  ...m973Points,
-  ...hPoints,
-  ...sPoints,
-  ...cPoints,
-  ...gPoints,
-  ...rPoints,
-  ...wPoints,
-  ...fPoints
-]
-
-const titresPointsReferences = [
-  ...mPointsReferences,
-  ...m973PointsReferences,
-  ...hPointsReferences,
-  ...sPointsReferences,
-  ...cPointsReferences,
-  ...gPointsReferences,
-  ...rPointsReferences,
-  ...wPointsReferences,
-  ...fPointsReferences
-]
-
-const titresDocuments = [
-  ...mDocuments,
-  ...m973Documents,
-  ...hDocuments,
-  ...sDocuments,
-  ...cDocuments,
-  ...gDocuments,
-  ...rDocuments,
-  ...wDocuments,
-  ...fDocuments
-]
-
-const titresTitulaires = [
-  ...mTitulaires,
-  ...m973Titulaires,
-  ...hTitulaires,
-  ...sTitulaires,
-  ...cTitulaires,
-  ...gTitulaires,
-  ...rTitulaires,
-  ...wTitulaires,
-  ...fTitulaires
-]
-
-const titresAmodiataires = [
-  ...mAmodiataires,
-  ...m973Amodiataires,
-  ...hAmodiataires,
-  ...sAmodiataires,
-  ...cAmodiataires,
-  ...gAmodiataires,
-  ...rAmodiataires,
-  ...wAmodiataires,
-  ...fAmodiataires
-]
-
-const titresEmprises = [
-  ...mEmprises,
-  ...m973Emprises,
-  ...hEmprises,
-  ...sEmprises,
-  ...cEmprises,
-  ...gEmprises,
-  ...rEmprises,
-  ...wEmprises,
-  ...fEmprises
-]
-
-const titresErreurs = [
-  ...mErreurs,
-  ...m973Erreurs,
-  ...hErreurs,
-  ...sErreurs,
-  ...cErreurs,
-  ...gErreurs,
-  ...rErreurs,
-  ...wErreurs,
-  ...fErreurs
-]
+const datas = files.reduce(
+  (d, file) =>
+    Object.assign(d, {
+      [`titres${file}`]: types.reduce((res, type) => {
+        const fileName = decamelize(`titres-${type}${file}`, '-')
+        return [...res, ...require(`../../sources/${fileName}.json`)]
+      }, [])
+    }),
+  {}
+)
 
 exports.seed = (knex, Promise) =>
   Promise.all([
@@ -244,18 +41,20 @@ exports.seed = (knex, Promise) =>
     )
     .then(() => knex('titresDemarches').del())
     .then(() => knex('titres').del())
-    .then(() => knex('titres').insert(titres))
-    .then(() => knex('titresDemarches').insert(titresDemarches))
-    .then(() => knex('titresEtapes').insert(titresEtapes))
+    .then(() => knex('titres').insert(datas.titres))
+    .then(() => knex('titresDemarches').insert(datas.titresDemarches))
+    .then(() => knex('titresEtapes').insert(datas.titresEtapes))
     .then(() =>
       Promise.all([
-        knex('titresSubstances').insert(titresSubstances),
-        knex('titresPoints').insert(titresPoints),
-        knex('titresEmprises').insert(titresEmprises),
-        knex('titresTitulaires').insert(titresTitulaires),
-        knex('titresAmodiataires').insert(titresAmodiataires),
-        knex('titresErreurs').insert(titresErreurs),
-        knex('titresDocuments').insert(titresDocuments)
+        knex('titresSubstances').insert(datas.titresSubstances),
+        knex('titresPoints').insert(datas.titresPoints),
+        knex('titresEmprises').insert(datas.titresEmprises),
+        knex('titresTitulaires').insert(datas.titresTitulaires),
+        knex('titresAmodiataires').insert(datas.titresAmodiataires),
+        knex('titresErreurs').insert(datas.titresErreurs),
+        knex('titresDocuments').insert(datas.titresDocuments)
       ])
     )
-    .then(() => knex('titresPointsReferences').insert(titresPointsReferences))
+    .then(() =>
+      knex('titresPointsReferences').insert(datas.titresPointsReferences)
+    )
