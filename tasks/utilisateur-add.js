aa5ddf001ed750f524e1ab6d459f6a92f7c9e4d6
Francois Romain <francoisromain@gmail.com>
2018-10-17 19:27:56 +0200

    fix:

--- tasks/utilisateur-add.js
+++ tasks/utilisateur-add.js
@@ -5,6 +5,8 @@ const { utilisateurAdd } = require('../postgres/queries/utilisateurs')
 
 const password = process.env.ADMIN_PASSWORD
 
+console.log(password)
+
 const utilisateur = {
   id: process.env.ADMIN_ID,
   email: process.env.ADMIN_EMAIL,
