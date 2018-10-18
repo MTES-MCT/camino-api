e99f533b178c68b12db27fb84518581e14a7a308
Francois Romain <francoisromain@gmail.com>
2018-10-18 16:18:00 +0200

    fix:

--- tasks/utilisateur-remove.js
+++ tasks/utilisateur-remove.js
@@ -5,8 +5,12 @@ const { utilisateurRemove } = require('../postgres/queries/utilisateurs')
 const id = process.env.ADMIN_ID
 
 const run = async () => {
-  await utilisateurRemove(id)
-  console.log('Utilisateur supprimé')
+  const utilisateur = await utilisateurRemove(id)
+  if (utilisateur) {
+    console.log('Utilisateur supprimé')
+  } else {
+    console.log('Aucun utilisateur supprimé')
+  }
   process.exit()
 }
 
