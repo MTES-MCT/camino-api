15cb0290321bebe69947d06def57abc56722b830
Francois Romain <francoisromain@gmail.com>
2018-11-05 16:51:32 +0100

    fix: migre les permissions avant les utilisateurs

--- _tools/knex/seeds/4-utilisateurs.js
+++ _tools/knex/seeds/4-utilisateurs.js
@@ -2,10 +2,6 @@ const utilisateurs = require('../../sources/utilisateurs.json')
 const permissions = require('../../sources/permissions.json')
 
 exports.seed = (knex, Promise) =>
-  Promise.all([knex('utilisateurs').del(), knex('permissions').del()]).then(
-    () =>
-      Promise.all([
-        knex('permissions').insert(permissions),
-        knex('utilisateurs').insert(utilisateurs)
-      ])
-  )
+  Promise.all([knex('utilisateurs').del(), knex('permissions').del()])
+    .then(() => knex('permissions').insert(permissions))
+    .then(() => knex('utilisateurs').insert(utilisateurs))
