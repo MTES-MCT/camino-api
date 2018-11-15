8db96553b7be5d576823b5dc422852a4c63e7945
Francois Romain <francoisromain@gmail.com>
2018-11-15 12:16:36 +0100

    refactor:

--- auth/token.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken')
-const { jwtSecret } = require('../config')
-const user = require('./user')
-
-const token = jwtSecret
-  ? jwt.sign(user, jwtSecret, { noTimestamp: true })
-  : null
-
-module.exports = token
