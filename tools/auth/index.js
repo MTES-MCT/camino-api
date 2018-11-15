908962641b5e461b098e1b426fba3a2583bd9a8f
Francois Romain <francoisromain@gmail.com>
2018-11-15 14:43:22 +0100

    refactor:

--- tools/auth/index.js
+++
@@ -1,6 +0,0 @@
-const jwt = require('jsonwebtoken')
-const { jwtSecret } = require('../config/index')
-
-const tokenCreate = (payload, options) => jwt.sign(payload, jwtSecret, options)
-
-module.exports = { tokenCreate }
