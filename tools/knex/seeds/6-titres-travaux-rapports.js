1f7521cba7a67368f30c71b0811007db0418adc3
Francois Romain <francoisromain@gmail.com>
2018-12-07 13:58:33 +0100

    fix: formate la date des rapports de travaux lors de l'import

--- tools/knex/seeds/6-titres-travaux-rapports.js
+++ tools/knex/seeds/6-titres-travaux-rapports.js
@@ -1,6 +1,14 @@
+const dateFormat = require('dateformat')
 const titresTravauxRapports = require('../../sources/titres-travaux-rapports.json')
 
 exports.seed = (knex, Promise) =>
   knex('titresTravauxRapports')
     .del()
-    .then(() => knex('titresTravauxRapports').insert(titresTravauxRapports))
+    .then(() =>
+      knex('titresTravauxRapports').insert(
+        titresTravauxRapports.map(tr => {
+          tr.date = dateFormat(tr.date, 'yyyy-mm-dd')
+          return tr
+        })
+      )
+    )
