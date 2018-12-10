5b8b5aab15ed36cda96e6ed1122feb6ac8b834bc
Francois Romain <francoisromain@gmail.com>
2018-12-10 20:47:19 +0100

    test(configure jest):

---
+++ tasks/_utils/titre-date-fin-find.test.js
@@ -0,0 +1,8 @@
+// const titreDateFinFind = require('./titre-date-fin-find')
+function sum(a, b) {
+  return a + b
+}
+test('test à écrire', () => {
+  // console.log('boum', main())
+  expect(sum(1, 2)).toBe(3)
+})
