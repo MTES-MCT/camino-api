cb3f0ed82e542566ef75b70d6804097c1f14fcc8
Francois Romain <francoisromain@gmail.com>
2018-12-14 12:51:24 +0100

    feat(tasks): ajoute une colonne annulationDemarcheId pour faire le lien avec une démarche d'annulati

---
+++ tools/utils/demarche.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+require('../../database/index')
+
+const {
+  titresDemarchesGet
+} = require('../../database/queries/titres-demarches')
+
+async function main() {
+  let res = await titresDemarchesGet({ demarchesIds: ['abr', 'ren', 'ret'] })
+
+  res = res.filter(({ typeId, etapes }) => {
+    if (typeId === 'ren') {
+      return !etapes.find(te => te.points.length)
+    }
+    return true
+  })
+
+  console.log(res.map(({ id, statutId }) => ({ id, statutId })))
+
+  process.exit()
+}
+
+main()
