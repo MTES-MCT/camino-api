459fa2a6bd524d7b2e76543e5a665636a85b9e38
Adrien Risser <risseraka@gmail.com>
2019-01-10 14:07:27 +0100

    feat(tasks): prends en compte les étapes 'mfr' pour les demandes initiales (#9)

--- tasks/_utils/titre-prop-etape-id-find.js
+++ tasks/_utils/titre-prop-etape-id-find.js
@@ -3,34 +3,39 @@
 // pour laquelle la propriété existe
 
 const titrePropEtapeIdFind = (titreDemarches, prop) =>
-  // filtre les démarches acceptée ou terminée
+  // filtre les démarches acceptée, terminée ou en instruction
   titreDemarches
-    .filter(titreDemarche => ['acc', 'ter'].includes(titreDemarche.statutId))
+    .filter(titreDemarche => ['acc', 'ter', 'ins'].includes(titreDemarche.statutId))
     // parcourt les démarches
     .reduce((etapeId, titreDemarche) => {
-      if (!etapeId) {
-        // filtre les étapes acceptation, fait ou favorable
-        const etape = titreDemarche.etapes
-          .filter(
-            titreEtape =>
-              ['acc', 'fai', 'fav'].includes(titreEtape.statutId) &&
-              ['dpu', 'dex'].includes(titreEtape.typeId)
-          )
-          // trouve une étape qui contient la propriété
-          .find(
-            titreEtape =>
-              (titreEtape[prop] &&
-                Array.isArray(titreEtape[prop]) &&
-                titreEtape[prop].length) ||
-              (titreEtape[prop] && !Array.isArray(titreEtape[prop]))
-          )
-
-        // si l'étape existe, retourne son id
-        // sinon retourne `null`
-        return (etape && etape.id) || (!etape && null)
+      // Si la démarche est en instruction,
+      // on ne prend en compte que l'octroi (demande initiale)
+      if (etapeId || titreDemarche.statutId === 'ins' && titreDemarche.typeId !== 'oct') {
+        return etapeId
       }
 
-      return etapeId
+      // filtre les étapes acceptation, fait ou favorable
+      const etape = titreDemarche.etapes
+        .filter(
+          titreEtape =>
+            ['acc', 'fai', 'fav'].includes(titreEtape.statutId) &&
+            (['dpu', 'dex', 'rpu'].includes(titreEtape.typeId) ||
+              // Si l'étape est une formalisation de la demande,
+              // on ne prend en compte que pour un octroi (demande initiale)
+              titreEtape.typeId === 'mfr' && titreDemarche.typeId === 'oct')
+        )
+        // trouve une étape qui contient la propriété
+        .find(
+          titreEtape =>
+            (titreEtape[prop] &&
+              Array.isArray(titreEtape[prop]) &&
+              titreEtape[prop].length) ||
+              (titreEtape[prop] && !Array.isArray(titreEtape[prop]))
+        )
+
+      // si l'étape existe, retourne son id
+      // sinon retourne `null`
+      return (etape && etape.id) || (!etape && null)
     }, null)
 
 module.exports = titrePropEtapeIdFind
