00c05061cf48729d8da2376e64fc01e4e6968278
Francois Romain <francoisromain@gmail.com>
2019-01-15 14:33:13 +0100

    feat(rapports): permet la modification d'un rapport de travaux

--- tools/export/tables/titres-travaux.js
+++ tools/export/tables/titres-travaux.js
@@ -5,7 +5,7 @@
 const tables = [
   {
     name: 'titresTravauxRapports',
-    columns: ['id', 'titreId', 'date', 'contenu'],
+    columns: ['id', 'titreId', 'date', 'confirmation', 'contenu'],
     callbacks: {
       contenu: v => JSON.stringify(v)
     }
--- tools/export/titre-travaux-rapport.js
+++ tools/export/titre-travaux-rapport.js
@@ -1,12 +1,20 @@
 const credentials = require('./credentials')
-const rowToSpreadsheetAdd = require('./_utils/row-to-spreadsheet-add')
+const spreadsheetRowAdd = require('./_utils/spreadsheet-row-add')
+const spreadsheetRowUpdate = require('./_utils/spreadsheet-row-update')
 const tables = require('./tables/titres-travaux')
 
 const titreTravauxRapportRowAdd = async content => {
   const spreadsheetId =
     process.env.GOOGLE_SPREADSHEET_ID_EXPORT_TITRES_TRAVAUX_RAPPORTS
 
-  await rowToSpreadsheetAdd(spreadsheetId, credentials, tables[0], content)
+  await spreadsheetRowAdd(spreadsheetId, credentials, tables[0], content)
 }
 
-module.exports = { titreTravauxRapportRowAdd }
+const titreTravauxRapportRowUpdate = async content => {
+  const spreadsheetId =
+    process.env.GOOGLE_SPREADSHEET_ID_EXPORT_TITRES_TRAVAUX_RAPPORTS
+
+  await spreadsheetRowUpdate(spreadsheetId, credentials, tables[0], content)
+}
+
+module.exports = { titreTravauxRapportRowAdd, titreTravauxRapportRowUpdate }
