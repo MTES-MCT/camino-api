c89f1b830949356b79393c578f8a21347e8ab3cc
Francois Romain <francoisromain@gmail.com>
2019-01-25 17:34:49 +0100

    build(doc): génère la documentation avec esdoc

--- tasks/_utils/titre-date-fin-find.js
+++ tasks/_utils/titre-date-fin-find.js
@@ -1,20 +1,22 @@
 const titreDemarcheDateFinAndDureeFind = require('./titre-demarche-date-fin-duree-find')
 
-// entrée:
-// - les démarches d'un titres
-// sortie:
-// - la date d'échéance du titre
+/**
+ * Trouve la date de fin d'un titre
+ * @param {Array} titreDemarches la liste des démarches d'un titres
+ * @return {String} la date de fin du titre sous forme yyyy-mm-dd
+ */
+
 const titreDateFinFind = titreDemarches => {
   // la dernière démarche dont le statut est acceptée ou terminée
   const titreDemarche = titreDemarches.find(titreDemarche =>
     ['acc', 'ter'].includes(titreDemarche.statutId)
   )
-
-  return (
+  const dateFin =
     titreDemarche &&
     titreDemarcheDateFinAndDureeFind(titreDemarches, titreDemarche.ordre)
       .dateFin
-  )
+
+  return dateFin
 }
 
 module.exports = titreDateFinFind
