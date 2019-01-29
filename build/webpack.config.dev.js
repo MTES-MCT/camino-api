ddc4d9da6bfb42bea974926505db09f0f90530b0
Francois Romain <francoisromain@gmail.com>
2019-01-29 16:04:26 +0100

    build(webpack): change le nom du bundle de dev en index.js

--- build/webpack.config.dev.js
+++ build/webpack.config.dev.js
@@ -5,7 +5,9 @@ const webpackConfigBase = require('./webpack.config.base')
 const webpackConfigDev = {
   mode: 'development',
   devtool: 'cheap-module-eval-source-map',
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js'
+  },
   plugins: [new webpackPluginNodemon()]
 }
 
