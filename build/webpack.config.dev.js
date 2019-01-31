cc560d803b664b0b4bb47be4847e1373ef2ca0de
Francois Romain <francoisromain@gmail.com>
2019-01-31 12:29:28 +0100

    build: compile avec typescript

--- build/webpack.config.dev.js
+++ build/webpack.config.dev.js
@@ -6,7 +6,7 @@ const webpackConfigDev = {
   mode: 'development',
   devtool: 'cheap-module-eval-source-map',
   entry: {
-    index: './src/index.js'
+    index: './src/index.ts'
   },
   plugins: [new webpackPluginNodemon()]
 }
