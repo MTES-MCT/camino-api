c4a12ec42ca454280be5640e2ba0179f1ca99ccc
Adrien Risser <risseraka@gmail.com>
2019-02-13 17:37:06 +0100

    refactor(tasks): évite un map filter map

--- src/tasks/titre-etapes.js
+++ src/tasks/titre-etapes.js
@@ -7,23 +7,26 @@ import {
 const titreEtapesOrdreUpdate = titreEtapesByDemarche =>
   titreEtapesByDemarche
     .sort((a, b) => a.date - b.date)
-    .map((titreEtape, index) => {
-      titreEtape.ordreNew = index + 1
-      return titreEtape
-    })
-    .filter(titreEtape => titreEtape.ordreNew !== titreEtape.ordre)
-    .map(titreEtape => {
-      const props = {
-        ordre: titreEtape.ordreNew
-      }
-
-      titreEtapeUpdate({ id: titreEtape.id, props }).then(u => {
-        console.log(
-          `Mise à jour: étape ${titreEtape.id}, ${JSON.stringify(props)}`
-        )
-        return u
-      })
-    })
+    .reduce(
+      (acc, titreEtape, index) =>
+        titreEtape.ordre === index + 1
+        ? acc
+        : [
+          ...acc,
+          titreEtapeUpdate({
+            id: titreEtape.id,
+            props: { ordre: index + 1 }
+          }).then(u => {
+            console.log(
+              `Mise à jour: étape ${titreEtape.id}, ${JSON.stringify({
+                ordre: index + 1
+              })}`
+            )
+            return u
+          })
+        ],
+      []
+    )
 
 const titreEtapeCommunesInsert = (titreEtape, communesIds) =>
   communesIds.reduce(
