86281451827008815eb4d9e35b76819e40663b6c
Francois Romain <francoisromain@gmail.com>
2019-02-28 17:49:06 +0100

    refactor: teste si le json est valide lors de l'appel à l'API-communes

--- src/tools/api-communes.js
+++ src/tools/api-communes.js
@@ -1,8 +1,16 @@
 import * as fetch from 'node-fetch'
+import * as geojsonhint from '@mapbox/geojsonhint'
 import errorLog from './error-log'
 
 const communesGeojsonGet = async geojson => {
+  const properties = JSON.stringify(geojson.properties)
+
   try {
+    const geojsonErrors = geojsonhint.hint(geojson)
+    if (geojsonErrors.length) {
+      throw { error: geojsonErrors.map(e => e.message) }
+    }
+
     const response = await fetch(process.env.GEO_API_URL, {
       method: 'post',
       headers: {
@@ -19,7 +27,7 @@ const communesGeojsonGet = async geojson => {
 
     return result
   } catch ({ error }) {
-    errorLog(`communesGeojsonGet ${JSON.stringify(geojson.properties)}`, error)
+    errorLog(`communesGeojsonGet ${properties}`, error)
   }
 }
 
