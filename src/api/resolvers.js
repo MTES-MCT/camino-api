ec4b79856308f042b31c72f6733121f790eb1fa0
Francois Romain <francoisromain@gmail.com>
2019-03-18 20:37:50 +0100

    fix: rend obligatoire le nom d'un établissement d'entreprise

--- src/api/resolvers.js
+++ src/api/resolvers.js
@@ -36,6 +36,7 @@ export default {
   //  types
   Json,
   GraphQLDate,
+
   //  queries
   version,
   titre,
@@ -49,6 +50,7 @@ export default {
   utilisateur,
   utilisateurs,
   permissions,
+
   // mutations
   titreAjouter,
   titreModifier,
