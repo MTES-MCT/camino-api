c44707be7ccee5afddf5b7ccb5bd7b4886d966ef
Francois Romain <francoisromain@gmail.com>
2019-03-21 20:07:08 +0100

    fix(entreprises): importe les dictionnaires de l'API insee

---
+++ src/tools/api-insee/voies.js
@@ -0,0 +1,170 @@
+export default [
+  {
+    id: 'ALL',
+    nom: 'Allée'
+  },
+  {
+    id: 'AV',
+    nom: 'Avenue'
+  },
+  {
+    id: 'BD',
+    nom: 'Boulevard'
+  },
+  {
+    id: 'CAR',
+    nom: 'Carrefour'
+  },
+  {
+    id: 'CHE',
+    nom: 'Chemin'
+  },
+  {
+    id: 'CHS',
+    nom: 'Chaussée'
+  },
+  {
+    id: 'CITE',
+    nom: 'Cité'
+  },
+  {
+    id: 'COR',
+    nom: 'Corniche'
+  },
+  {
+    id: 'CRS',
+    nom: 'Cours'
+  },
+  {
+    id: 'DOM',
+    nom: 'Domaine'
+  },
+  {
+    id: 'DSC',
+    nom: 'Descente'
+  },
+  {
+    id: 'ECA',
+    nom: 'Ecart'
+  },
+  {
+    id: 'ESP',
+    nom: 'Esplanade'
+  },
+  {
+    id: 'FG',
+    nom: 'Faubourg'
+  },
+  {
+    id: 'GR',
+    nom: 'Grande Rue'
+  },
+  {
+    id: 'HAM',
+    nom: 'Hameau'
+  },
+  {
+    id: 'HLE',
+    nom: 'Halle'
+  },
+  {
+    id: 'IMP',
+    nom: 'Impasse'
+  },
+  {
+    id: 'LD',
+    nom: 'Lieu-dit'
+  },
+  {
+    id: 'LOT',
+    nom: 'Lotissement'
+  },
+  {
+    id: 'MAR',
+    nom: 'Marché'
+  },
+  {
+    id: 'MTE',
+    nom: 'Montée'
+  },
+  {
+    id: 'PAS',
+    nom: 'Passage'
+  },
+  {
+    id: 'PL',
+    nom: 'Place'
+  },
+  {
+    id: 'PLN',
+    nom: 'Plaine'
+  },
+  {
+    id: 'PLT',
+    nom: 'Plateau'
+  },
+  {
+    id: 'PRO',
+    nom: 'Promenade'
+  },
+  {
+    id: 'PRV',
+    nom: 'Parvis'
+  },
+  {
+    id: 'QUA',
+    nom: 'Quartier'
+  },
+  {
+    id: 'QUAI',
+    nom: 'Quai'
+  },
+  {
+    id: 'RES',
+    nom: 'Résidence'
+  },
+  {
+    id: 'RLE',
+    nom: 'Ruelle'
+  },
+  {
+    id: 'ROC',
+    nom: 'Rocade'
+  },
+  {
+    id: 'RPT',
+    nom: 'Rond-point'
+  },
+  {
+    id: 'RTE',
+    nom: 'Route'
+  },
+  {
+    id: 'RUE',
+    nom: 'Rue'
+  },
+  {
+    id: 'SEN',
+    nom: 'Sente - Sentier'
+  },
+  {
+    id: 'SQ',
+    nom: 'Square'
+  },
+  {
+    id: 'TPL',
+    nom: 'Terre-plein'
+  },
+  {
+    id: 'TRA',
+    nom: 'Traverse'
+  },
+  {
+    id: 'VLA',
+    nom: 'Villa'
+  },
+  {
+    id: 'VLGE',
+    nom: 'Village'
+  }
+]
