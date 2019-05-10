4c9f534e66e32937d706be4c3b68cd02d6398032
François Romain <francoisromain@gmail.com>
2019-05-10 11:38:00 +0200

    refactor(fichiers): modifie la logique de téléchargement des documents (#59)
    * refactor(fichiers): modifie la logique de téléchargement des documents

---
+++ src/api/resolvers/documents.js
@@ -0,0 +1,42 @@
+import auth from './_auth'
+import { titreDocumentGet } from '../../database/queries/titres-documents'
+import { titreEtapeGet } from '../../database/queries/titres-etapes'
+import { titreDemarcheGet } from '../../database/queries/titres-demarches'
+import { titreGet } from '../../database/queries/titres'
+import { utilisateurGet } from '../../database/queries/utilisateurs'
+
+const documentNameGet = async (userId, titreDocumentId) => {
+  if (!titreDocumentId) {
+    throw new Error('id du document absent')
+  }
+
+  const titreDocument = await titreDocumentGet(titreDocumentId)
+
+  if (!titreDocument || !titreDocument.fichier) {
+    throw new Error('fichier inexistant')
+  }
+
+  if (titreDocument.public) return `${titreDocument.fichier}.pdf`
+
+  if (!userId) {
+    throw new Error('permissions insuffisantes')
+  }
+
+  const user = await utilisateurGet(userId)
+
+  if (!user) {
+    throw new Error('utilisateur inexistant')
+  }
+
+  const titreEtape = await titreEtapeGet(titreDocument.titreEtapeId)
+  const titreDemarche = await titreDemarcheGet(titreEtape.titreDemarcheId)
+  const titre = await titreGet(titreDemarche.titreId)
+
+  if (!auth(user, titre, ['admin', 'super', 'editeur'])) {
+    throw new Error('permissions insuffisantes')
+  }
+
+  return `${titreDocument.fichier}.pdf`
+}
+
+export { documentNameGet }
