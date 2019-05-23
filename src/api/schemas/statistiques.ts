e8c208d07ed2e60f09328de05cf484d29ffda82e
Francois Romain <francoisromain@gmail.com>
2019-05-23 10:51:34 +0200

    fix: renomme une occurence de 'stats' qui avait été oubliée

--- src/api/schemas/statistiques.ts
+++ src/api/schemas/statistiques.ts
@@ -1,11 +1,11 @@
 export default `
 "Statistiques"
 type Statistiques {
-    titresTotal: Int
-    titresValide: Int 
-    titresActivites2018Depose: Int
-    titreActivites2018Ratio: Int
-    titresActivites2018BeneficesEntreprise: Int
-    titresActivites2018BeneficesAdministration: Int
+  titresTotal: Int
+  titresValide: Int 
+  titresActivites2018Depose: Int
+  titreActivites2018Ratio: Int
+  titresActivites2018BeneficesEntreprise: Int
+  titresActivites2018BeneficesAdministration: Int
 }
 `
