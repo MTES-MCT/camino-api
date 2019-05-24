4ad1bab08e4bb87c642dba37759d61148ba2ece7
florent chalvet <florentchalvet@gmail.com>
2019-05-24 18:38:33 +0200

    style:

--- src/tools/dev/database/documents-manquants.js
+++ src/tools/dev/database/documents-manquants.js
@@ -4,6 +4,7 @@ import '../../../database/index'
 import { titresDocumentsGet } from '../../../database/queries/titres-documents'
 
 import * as fs from 'fs'
+import * as path from 'path'
 
 async function main() {
   const titresDocuments = await titresDocumentsGet()
@@ -14,30 +15,28 @@ async function main() {
   )
 
   const pdfFiles = fs.readdirSync('././././files')
-  const pdfNames = pdfFiles.map(pdfFile => pdfFile.split('.')[0])
+  const pdfNames = pdfFiles.map(pdfFile => path.basename(pdfFile))
 
   const pdfMiss = pdfNames.filter(
     pdfName => titresDocumentsListe.indexOf(pdfName) === -1
   )
 
-  if (pdfMiss.length === 0) {
-    console.log('Tous les pdfs sont présents dans la base de données')
-  } else {
-    let texte = `Il manque les pdfs suivants dans la base de données:`
-    pdfMiss.forEach(pdf => (texte += `\n- ${pdf}`))
-    console.log(texte)
-  }
+  console.log(
+    pdfMiss.length === 0
+      ? 'Tous les pdfs sont présents dans la base de données'
+      : `Il manque les pdfs suivants dans la base de données:`
+  )
+  console.log(pdfMiss.map(pdf => `- ${pdf}`).join('\n'))
 
   const titresDocumentsMiss = titresDocumentsListe.filter(
     titresDocumentsName => pdfNames.indexOf(titresDocumentsName) === -1
   )
-  if (titresDocumentsMiss.length === 0) {
-    console.log('Tous les documents sont présents dans les fichiers')
-  } else {
-    console.log(
-      `Il manque ${titresDocumentsMiss.length} documents dans les fichiers`
-    )
-  }
+  console.log(
+    titresDocumentsMiss.length === 0
+      ? 'Tous les documents sont présents dans les fichiers'
+      : `Il manque ${titresDocumentsMiss.length} documents dans les fichiers`
+  )
+  console.log(titresDocumentsMiss.map(titres => `- ${titres}`).join('\n'))
 
   process.exit(0)
 }
