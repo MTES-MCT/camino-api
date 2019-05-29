0e6acc177f69006258b62b314a1bf2b86588ad6c
FChalvet <44586774+FChalvet@users.noreply.github.com>
2019-05-29 10:21:23 +0200

    feat(tools): vérifie que les documents sont présents en base #80

--- src/database/queries/titres-documents.js
+++ src/database/queries/titres-documents.js
@@ -3,4 +3,6 @@ import TitresDocuments from '../models/titres-documents'
 const titreDocumentGet = async titreDocumentId =>
   TitresDocuments.query().findById(titreDocumentId)
 
-export { titreDocumentGet }
+const titresDocumentsGet = async () => TitresDocuments.query()
+
+export { titreDocumentGet, titresDocumentsGet }
