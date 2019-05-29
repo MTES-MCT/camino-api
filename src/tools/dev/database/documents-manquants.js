e3e31070f9bcd3f0f27f499b51f49e65b001a2ae
FChalvet <44586774+FChalvet@users.noreply.github.com>
2019-05-29 10:21:23 +0200

    feat(tools): vérifie que les documents sont présents en base #80

---
+++ src/tools/dev/database/documents-manquants.js
@@ -0,0 +1,54 @@
+import 'dotenv/config'
+import '../../../database/index'
+
+import { titresDocumentsGet } from '../../../database/queries/titres-documents'
+
+import * as fs from 'fs'
+import * as path from 'path'
+
+async function main() {
+  const titresDocuments = await titresDocumentsGet()
+  const titresDocumentsFichiers = titresDocuments.reduce(
+    (arr, titreDocument) =>
+      titreDocument.fichier ? [...arr, titreDocument.fichier] : arr,
+    []
+  )
+
+  const pdfFiles = fs.readdirSync('././././files')
+  const pdfNames = pdfFiles.map(pdfFile => path.basename(pdfFile, '.pdf'))
+
+  const pdfMissing = pdfNames.filter(
+    pdfName => titresDocumentsFichiers.indexOf(pdfName) === -1
+  )
+
+  if (pdfMissing.length === 0) {
+    console.log('Tous les pdfs sont présents dans la base de données')
+  } else {
+    console.log(
+      `${pdfMissing.length} pdfs n'existent pas dans la base de données`
+    )
+    console.log(pdfMissing.map(pdf => `- ${pdf}`).join('\n'))
+  }
+
+  const titreDocumentsFichiersMissing = titresDocumentsFichiers.filter(
+    titresDocumentsName => pdfNames.indexOf(titresDocumentsName) === -1
+  )
+  if (titreDocumentsFichiersMissing.length === 0) {
+    console.log(
+      'Tous les noms de fichiers renseignés en base de données existent'
+    )
+  } else {
+    console.log(
+      `${
+        titreDocumentsFichiersMissing.length
+      } noms de fichiers renseignés en base de données n'existent pas`
+    )
+    console.log(
+      titreDocumentsFichiersMissing.map(titres => `- ${titres}`).join('\n')
+    )
+  }
+
+  process.exit(0)
+}
+
+main()
