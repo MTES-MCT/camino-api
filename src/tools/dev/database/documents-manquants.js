bc1ec7484e3c887bd852ad8f82cd22d2e3543e42
Florent Chalvet <florentchalvet@gmail.com>
2019-05-29 09:42:34 +0200

    refactor:

--- src/tools/dev/database/documents-manquants.js
+++ src/tools/dev/database/documents-manquants.js
@@ -21,22 +21,24 @@ async function main() {
     pdfName => titresDocumentsListe.indexOf(pdfName) === -1
   )
 
-  console.log(
-    pdfMiss.length === 0
-      ? 'Tous les pdfs sont présents dans la base de données'
-      : `Il manque les pdfs suivants dans la base de données:`
-  )
-  console.log(pdfMiss.map(pdf => `- ${pdf}`).join('\n'))
+  if (pdfMiss.length === 0) {
+    console.log('Tous les pdfs sont présents dans la base de données')
+  } else {
+    console.log(`Il manque ${pdfMiss.length} pdfs  dans la base de données`)
+    console.log(pdfMiss.map(pdf => `- ${pdf}`).join('\n'))
+  }
 
   const titresDocumentsMiss = titresDocumentsListe.filter(
     titresDocumentsName => pdfNames.indexOf(titresDocumentsName) === -1
   )
-  console.log(
-    titresDocumentsMiss.length === 0
-      ? 'Tous les documents sont présents dans les fichiers'
-      : `Il manque ${titresDocumentsMiss.length} documents dans les fichiers`
-  )
-  console.log(titresDocumentsMiss.map(titres => `- ${titres}`).join('\n'))
+  if (titresDocumentsMiss.length === 0) {
+    console.log('Tous les documents sont présents dans les fichiers')
+  } else {
+    console.log(
+      `Il manque ${titresDocumentsMiss.length} documents dans les fichiers`
+    )
+    console.log(titresDocumentsMiss.map(titres => `- ${titres}`).join('\n'))
+  }
 
   process.exit(0)
 }
