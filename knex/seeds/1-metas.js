57a55e8522d2bdca645cd591151160fcc5c56f97
Adrien Risser <risseraka@gmail.com>
2019-06-04 10:01:24 +0200

    fix(migrate): corrige la longueur des messages d'erreurs du seed lors de la migration

--- knex/seeds/1-metas.js
+++ knex/seeds/1-metas.js
@@ -1,3 +1,5 @@
+const seeding = require('../seeding')
+
 const domaines = require('../../sources/domaines.json')
 const types = require('../../sources/types.json')
 // eslint-disable-next-line camelcase
@@ -21,53 +23,47 @@ const geoSystemes = require('../../sources/geo-systemes.json')
 const devises = require('../../sources/devises.json')
 const volumeUnites = require('../../sources/volume-unites.json')
 
-exports.seed = (knex, Promise) =>
-  Promise.all([
-    knex('domaines__types').del(),
-    knex('demarchesTypes__types').del(),
-    knex('demarchesTypes__etapesTypes').del(),
-    knex('demarchesTypes__demarchesStatuts').del(),
-    knex('etapesTypes__etapesStatuts').del(),
-    knex('statuts').del(),
-    knex('emprises').del(),
-    knex('geoSystemes').del(),
-    knex('devises').del(),
-    knex('volumeUnites').del()
+exports.seed = seeding(async ({ del, insert }) => {
+  await Promise.all([
+    del('domaines__types'),
+    del('demarchesTypes__types'),
+    del('demarchesTypes__etapesTypes'),
+    del('demarchesTypes__demarchesStatuts'),
+    del('etapesTypes__etapesStatuts'),
+    del('statuts'),
+    del('emprises'),
+    del('geoSystemes'),
+    del('devises'),
+    del('volumeUnites')
   ])
-    .then(() =>
-      Promise.all([
-        knex('phasesStatuts').del(),
-        knex('demarchesTypes').del(),
-        knex('etapesTypes').del(),
-        knex('demarchesStatuts').del(),
-        knex('etapesStatuts').del()
-      ])
-    )
-    .then(() => Promise.all([knex('domaines').del(), knex('types').del()]))
-    .then(() =>
-      Promise.all([
-        knex('domaines').insert(domaines),
-        knex('types').insert(types),
-        knex('statuts').insert(statuts),
-        knex('phasesStatuts').insert(phasesStatuts),
-        knex('demarchesTypes').insert(demarchesTypes),
-        knex('etapesTypes').insert(etapesTypes),
-        knex('emprises').insert(emprises),
-        knex('demarchesStatuts').insert(demarchesStatuts),
-        knex('etapesStatuts').insert(etapesStatuts),
-        knex('geoSystemes').insert(geoSystemes),
-        knex('devises').insert(devises),
-        knex('volumeUnites').insert(volumeUnites)
-      ])
-    )
-    .then(() =>
-      Promise.all([
-        knex('domaines__types').insert(domaines_types),
-        knex('demarchesTypes__types').insert(demarchesTypes_types),
-        knex('demarchesTypes__demarchesStatuts').insert(
-          demarchesTypes_demarchesStatuts
-        ),
-        knex('demarchesTypes__etapesTypes').insert(demarchesTypes_etapesTypes),
-        knex('etapesTypes__etapesStatuts').insert(etapesTypes_etapesStatuts)
-      ])
-    )
+  await Promise.all([
+    del('phasesStatuts'),
+    del('demarchesTypes'),
+    del('etapesTypes'),
+    del('demarchesStatuts'),
+    del('etapesStatuts')
+  ])
+  await Promise.all([del('domaines'), del('types')])
+
+  await Promise.all([
+    insert('domaines', domaines),
+    insert('types', types),
+    insert('statuts', statuts),
+    insert('phasesStatuts', phasesStatuts),
+    insert('demarchesTypes', demarchesTypes),
+    insert('etapesTypes', etapesTypes),
+    insert('emprises', emprises),
+    insert('demarchesStatuts', demarchesStatuts),
+    insert('etapesStatuts', etapesStatuts),
+    insert('geoSystemes', geoSystemes),
+    insert('devises', devises),
+    insert('volumeUnites', volumeUnites)
+  ])
+  await Promise.all([
+    insert('domaines__types', domaines_types),
+    insert('demarchesTypes__types', demarchesTypes_types),
+    insert('demarchesTypes__demarchesStatuts', demarchesTypes_demarchesStatuts),
+    insert('demarchesTypes__etapesTypes', demarchesTypes_etapesTypes),
+    insert('etapesTypes__etapesStatuts', etapesTypes_etapesStatuts)
+  ])
+})
--- knex/seeds/3-territoires.js
+++ knex/seeds/3-territoires.js
@@ -1,17 +1,19 @@
+const seeding = require('../seeding')
+
 const pays = require('../../sources/pays.json')
 const regions = require('../../sources/regions.json')
 const departements = require('../../sources/departements.json')
 // const communes = require('../../sources/communes.json')
 
-exports.seed = (knex, Promise) =>
-  // knex('communes')
-  //   .del()
-  knex('communes')
-    .del()
-    .then(() => knex('departements').del())
-    .then(() => knex('regions').del())
-    .then(() => knex('pays').del())
-    .then(() => knex('pays').insert(pays))
-    .then(() => knex('regions').insert(regions))
-    .then(() => knex('departements').insert(departements))
-// .then(() => knex('communes').insert(communes))
+exports.seed = seeding(async ({ del, insert }) => {
+  // await del('communes')
+  await del('communes')
+  await del('departements')
+  await del('regions')
+  await del('pays')
+
+  await insert('pays', pays)
+  await insert('regions', regions)
+  await insert('departements', departements)
+  // await insert('communes', communes)
+})
