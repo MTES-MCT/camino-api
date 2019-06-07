41e6ea39be5d06aeea10c1a72ffdc432c021bf3f
Adrien Risser <risseraka@gmail.com>
2019-06-07 20:24:37 +0200

    fix(migrate): corrige l'insertion de plus de 10 000 éléments (#85)

--- knex/seeding.js
+++ knex/seeding.js
@@ -1,3 +1,5 @@
+const INSERT_SIZE = 5000
+
 module.exports = func => knex => {
   const del = table => {
     console.info(`Suppression des données de la table "${table}"`)
@@ -12,16 +14,19 @@ module.exports = func => knex => {
       } élément(s))`
     )
 
-    return knex(table)
-      .insert(data)
-      .catch(e => {
-        // Récupère le message d'erreur provenant de la base de données
-        // ex : `value too long for type character varying(255)`
-        const problem = e.message.split(' - ').pop()
-        const message = `Table "${table}" - ${problem}`
+    const arrs = []
+    for (let i = 0, j = data.length; i < j; i += INSERT_SIZE) {
+      arrs.push(data.slice(i, i + INSERT_SIZE))
+    }
 
-        throw new Error(message)
-      })
+    return Promise.all(arrs.map(arr => knex(table).insert(arr))).catch(e => {
+      // Si le message d'erreur est trop long
+      // Réduit la taille du message d'erreur à 100 caracters
+      const problem = e.message.split(' - ').pop()
+      const message = `Table "${table}" - ${problem}`
+
+      throw new Error(message)
+    })
   }
 
   return func({ del, insert })
