e2925986f6bea11c694951e878f6144b91649aa7
Francois Romain <francoisromain@gmail.com>
2019-06-26 10:33:23 +0200

    refactor:

--- src/api/resolvers/_eager.js
+++ src/api/resolvers/_eager.js
@@ -2,38 +2,40 @@ import * as graphqlFields from 'graphql-fields'
 
 // in: objet {cle1: { id: {}}, cle2: {id: {}}}
 // out: array ["cle1", "cle2"]
-const fieldsToArray = (obj, format) => {
-  return Object.keys(obj).reduce((acc, key) => {
+const fieldsToArray = (fields, format) => {
+  return Object.keys(fields).reduce((acc, id) => {
+    const fieldsSub = fields[id]
     // supprime
     // - les propriétés qui n'ont pas d'enfants
     // - les propriétés dont le nom commence par '$' (ex: $modifier)
-    if (Object.keys(obj[key]).length === 0 || key.slice(0, 1) === '$')
-      return acc
+    if (Object.keys(fieldsSub).length === 0 || id[0] === '$') return acc
 
     // formate les propriétés enfants récursivement
-    const fieldsSub = fieldsAstToEagerString(obj[key], key, format)
+    const fieldsSubString = fieldsToString(fieldsSub, id, format)
 
     // ajoute un modifieur à certaines propriétés
-    if (obj[key].$modifier) {
-      key = `${key}(${obj[key].$modifier})`
+    if (fieldsSub.$modifier) {
+      id = `${id}(${fieldsSub.$modifier})`
     }
 
     // si la propriété a des enfants
     // on les ajoute à la chaine
-    if (fieldsSub) {
-      key = `${key}.${fieldsSub}`
+    if (fieldsSubString) {
+      id = `${id}.${fieldsSubString}`
     }
 
-    return [...acc, key]
+    return [...acc, id]
   }, [])
 }
 
 // in: objet {cle1: { id: {}}, cle2: {cle3: {id: {}}, cle4: {id: {}}}}
 // out: string "[cle1, cle2.[cle3, cle4]]"
-const fieldsAstToEagerString = (obj, parent, format) => {
-  const fields = fieldsToArray(format(obj, parent), format)
+const fieldsToString = (fields, parent, format) => {
+  const fieldsArray = fieldsToArray(format(fields, parent), format)
 
-  return fields.length > 1 ? `[${fields.join(', ')}]` : fields.toString()
+  return fieldsArray.length > 1
+    ? `[${fieldsArray.join(', ')}]`
+    : fieldsArray.toString()
 }
 
 // optimise la requête Sql en demandant uniquement les champs
@@ -42,18 +44,14 @@ const fieldsAstToEagerString = (obj, parent, format) => {
 // - info: objet contenant les propriétés de la requête graphQl
 // - format: fonction qui transforme l'objet
 // out: string de eager pour la requête avec objection.js
-const eagerBuild = (info, format = (obj, parent) => obj) => {
+const eagerBuild = (info, format = (fields, parent) => fields) => {
   // transforme la requête graphQl en un AST
-  // qui défini tous les champs requis par le client
-  const graphQlFieldsAst = graphqlFields(
-    info,
-    {},
-    { excludedFields: ['__typename'] }
-  )
+  // qui défini les champs requis par le client
+  const fields = graphqlFields(info, {}, { excludedFields: ['__typename'] })
 
-  // transforme l'ast de la requête GraphQl
-  // en une string pour renseigner le eager de objection.js
-  return fieldsAstToEagerString(graphQlFieldsAst, 'root', format)
+  // in: objet ast de la requête GraphQl
+  // out: string au format 'eager' de objection.js
+  return fieldsToString(fields, 'root', format)
 }
 
 export default eagerBuild
