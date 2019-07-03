da618cb2647702ecab93a52ae937caa83b124107
François Romain <francoisromain@gmail.com>
2019-07-04 00:26:33 +0200

    refactor: importe des fichiers .graphql au lieu de strings js (#115)

--- src/api/schemas.ts
+++ src/api/schemas.ts
@@ -1,48 +1,9 @@
 import { buildSchema } from 'graphql'
+import { importSchema } from 'graphql-import'
+import { join } from 'path'
 
-import index from './schemas/index'
-import metas from './schemas/metas'
-import metasActivites from './schemas/metas-activites'
-import territoires from './schemas/territoires'
-import calendrier from './schemas/calendrier'
-import titres from './schemas/titres'
-import substances from './schemas/substances'
-import geojson from './schemas/geojsons'
-import points from './schemas/points'
-import utilisateurs from './schemas/utilisateurs'
-import administrations from './schemas/administrations'
-import entreprises from './schemas/entreprises'
-import titresActivites from './schemas/titres-activites'
-import statistiques from './schemas/statistiques'
+const index = importSchema(join(__dirname, 'schemas/index.graphql'))
 
-const schema = buildSchema(`
-  ${index}
-
-  ${metas}
-
-  ${metasActivites}
-
-  ${territoires}
-
-  ${calendrier}
-
-  ${titres}
-
-  ${substances}
-
-  ${geojson}
-
-  ${points}
-
-  ${utilisateurs}
-
-  ${administrations}
-
-  ${entreprises}
-
-  ${titresActivites}
-
-  ${statistiques}
-`)
+const schema = buildSchema(`${index}`)
 
 export default schema
