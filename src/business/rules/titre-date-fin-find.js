415b1ddfe9b6e11b13821ec4b685c65b45487fe0
Francois Romain <francoisromain@gmail.com>
2019-07-05 01:16:01 +0200

    fix: ajoute un type manquant

--- src/business/rules/titre-date-fin-find.js
+++ src/business/rules/titre-date-fin-find.js
@@ -2,9 +2,10 @@ import * as dateFormat from 'dateformat'
 import titreDemarcheDateFinAndDureeFind from './titre-demarche-date-fin-duree-find'
 
 /**
- * Trouve la date de fin d'un titre
- * @param {titreDemarche[]} titreDemarches la liste des démarches d'un titre
- * @return {String} la date de fin du titre sous forme yyyy-mm-dd
+ * Trouve la date de fin d'un titre.
+ *
+ * @param {titreDemarche[]} titreDemarches - La liste des démarches d'un titre.
+ * @returns {string} La date de fin du titre sous forme yyyy-mm-dd.
  */
 
 const titreDateFinFind = titreDemarches => {
