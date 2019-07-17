d8b06ce3207e196bb8c5772d017752a5ab7d1d32
François Romain <francoisromain@gmail.com>
2019-07-17 17:58:18 +0200

    fix: exporte les activités lorsque l'id d'un titre est modifiée (#129)

--- src/business/processes/titre-id-update.js
+++ src/business/processes/titre-id-update.js
@@ -1,11 +1,23 @@
 import titreIdAndRelationsUpdate from '../utils/titre-id-and-relations-update'
 import { titreIdsUpdate } from '../queries/titres'
+import { titreActivitesRowUpdate } from '../../tools/export/titre-activites'
 
 const titreIdUpdate = async titreOld => {
   const { titreNew, hasChanged } = titreIdAndRelationsUpdate(titreOld)
   if (!hasChanged) return titreOld
 
   const titreIdUpdate = await titreIdsUpdate(titreOld.id, titreNew)
+
+  // met à jour toutes les activités dans la spreadsheet
+  if (
+    titreOld.id !== titreNew.id &&
+    titreNew.activites &&
+    titreNew.activites.length
+  ) {
+    const idGet = titreActiviteId =>
+      titreActiviteId.replace(titreNew.id, titreOld.id)
+    await titreActivitesRowUpdate(titreNew.activites, idGet)
+  }
 
   console.log(titreIdUpdate)
 
