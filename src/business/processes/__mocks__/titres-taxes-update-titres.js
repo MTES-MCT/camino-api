6cd67e763146be9359e52f60e9d8b9837779b330
florent chalvet <florentchalvet@gmail.com>
2019-08-20 19:04:06 +0200

    test: complete les tests sur les taxes

---
+++ src/business/processes/__mocks__/titres-taxes-update-titres.js
@@ -0,0 +1,8 @@
+const titresTaxe = [
+  {
+    id: 'h-cxx-courdemanges-1988',
+    taxes: []
+  }
+]
+
+export { titresTaxe }
--- src/business/processes/titres-taxes-update.test.js
+++ src/business/processes/titres-taxes-update.test.js
@@ -4,7 +4,7 @@ import * as titreTaxesTypesFilter from '../utils/titre-activites-filter'
 import * as titreTaxesQueries from '../../database/queries/titres-taxes'
 import * as titreTaxesBuild from '../rules/titre-taxes-build'
 
-import { titresToutesActivites } from './__mocks__/titres-activites-update-titres'
+import { titresTaxe } from './__mocks__/titres-taxes-update-titres'
 
 jest.mock('../utils/titre-activites-filter', () => ({
   default: jest.fn()
@@ -23,12 +23,37 @@ describe("ajout de la taxe d'un titre", () => {
     titreTaxesTypesFilter.default.mockImplementation(() => [1])
     titreTaxesBuild.default.mockImplementation(() => [1])
 
-    const log = await titresTaxesUpdate(titresToutesActivites, [])
+    const log = await titresTaxesUpdate(titresTaxe, [])
 
     expect(log).toEqual('Mise à jour: 1 taxe(s) de titres.')
 
     expect(titreTaxesTypesFilter.default).toHaveBeenCalled()
-    expect(titreTaxesQueries.titreTaxesUpsert).toHaveBeenCalled()
     expect(titreTaxesBuild.default).toHaveBeenCalled()
+    expect(titreTaxesQueries.titreTaxesUpsert).toHaveBeenCalled()
+  })
+
+  test('le titre ne correspond à aucun type de taxe', async () => {
+    titreTaxesTypesFilter.default.mockImplementation(() => [])
+
+    const log = await titresTaxesUpdate(titresTaxe, [])
+
+    expect(log).toEqual('Mise à jour: 0 taxe(s) de titres.')
+
+    expect(titreTaxesTypesFilter.default).toHaveBeenCalled()
+    expect(titreTaxesBuild.default).not.toHaveBeenCalled()
+    expect(titreTaxesQueries.titreTaxesUpsert).not.toHaveBeenCalled()
+  })
+
+  test('le titre ne possède pas de taxe affilié', async () => {
+    titreTaxesTypesFilter.default.mockImplementation(() => [1])
+    titreTaxesBuild.default.mockImplementation(() => [])
+
+    const log = await titresTaxesUpdate(titresTaxe, [])
+
+    expect(log).toEqual('Mise à jour: 0 taxe(s) de titres.')
+
+    expect(titreTaxesTypesFilter.default).toHaveBeenCalled()
+    expect(titreTaxesBuild.default).toHaveBeenCalled()
+    expect(titreTaxesQueries.titreTaxesUpsert).not.toHaveBeenCalled()
   })
 })
