c7ae7591f1f35f6756ddf40d2949c92f08e5a2b2
Adrien Risser <risseraka@gmail.com>
2019-10-04 13:20:22 +0200

    fix(business): corrige l'ordre des étapes et des démarches pendant le calcul des propriétés de titres

--- src/business/rules/titre-prop-etape-id-find.js
+++ src/business/rules/titre-prop-etape-id-find.js
@@ -1,12 +1,15 @@
 // retourne l'id de la dernière étape acceptée
 // de la dernière démarche acceptée
 // pour laquelle la propriété existe
+import titreDemarchesAscSort from '../utils/titre-demarches-asc-sort'
+import titreEtapesDescSort from '../utils/titre-etapes-desc-sort'
 
 const titrePropEtapeIdFind = (
   { demarches: titreDemarches, statutId: titreStatutId },
   prop
 ) =>
-  titreDemarches
+  titreDemarchesAscSort(titreDemarches)
+    .reverse()
     // parcourt les démarches
     .reduce((etapeId, titreDemarche) => {
       // si une étape a déjà été trouvée
@@ -31,46 +34,48 @@ const titrePropEtapeIdFind = (
         return etapeId
       }
 
-      const etape = titreDemarche.etapes.find(titreEtape => {
-        // trouve une étape qui contient la propriété
-        const isPropFound =
-          titreEtape[prop] &&
-          (!Array.isArray(titreEtape[prop]) ||
-            // la propriété ne doit pas être vide si c'est un tableau
-            titreEtape[prop].length)
-        if (!isPropFound) return false
+      const etape = titreEtapesDescSort(titreDemarche.etapes).find(
+        titreEtape => {
+          // trouve une étape qui contient la propriété
+          const isPropFound =
+            titreEtape[prop] &&
+            (!Array.isArray(titreEtape[prop]) ||
+              // la propriété ne doit pas être vide si c'est un tableau
+              titreEtape[prop].length)
+          if (!isPropFound) return false
 
-        // filtre les étapes acceptation, fait ou favorable
-        const isEtapeValide =
-          ['acc', 'fai', 'fav'].includes(titreEtape.statutId) &&
-          // Si la démarche est un octroi (demande initiale)
-          // on prend en compte n'importe quelle étape
-          (['oct', 'vut', 'vct'].includes(titreDemarche.typeId) ||
-            // ou si on cherche le périmètre
-            // et que le titre est en modification en instance
-            (prop.match('point') && titreStatutId === 'mod') ||
-            // sinon, on ne prend en compte que les étapes de décision
-            ['dpu', 'rpu', 'dex', 'dim', 'sco', 'def'].includes(
-              titreEtape.typeId
-            ))
-        if (!isEtapeValide) return false
+          // filtre les étapes acceptation, fait ou favorable
+          const isEtapeValide =
+            ['acc', 'fai', 'fav'].includes(titreEtape.statutId) &&
+            // Si la démarche est un octroi (demande initiale)
+            // on prend en compte n'importe quelle étape
+            (['oct', 'vut', 'vct'].includes(titreDemarche.typeId) ||
+              // ou si on cherche le périmètre
+              // et que le titre est en modification en instance
+              (prop.match('point') && titreStatutId === 'mod') ||
+              // sinon, on ne prend en compte que les étapes de décision
+              ['dpu', 'rpu', 'dex', 'dim', 'sco', 'def'].includes(
+                titreEtape.typeId
+              ))
+          if (!isEtapeValide) return false
 
-        if (prop.match('amodiataires')) {
-          const { dateFin } = titreEtape
+          if (prop.match('amodiataires')) {
+            const { dateFin } = titreEtape
 
-          // si la date de fin de l'étape est passée
-          // l'amodiataire n'est plus valide
-          if (dateFin && new Date(dateFin) < new Date()) return false
+            // si la date de fin de l'étape est passée
+            // l'amodiataire n'est plus valide
+            if (dateFin && new Date(dateFin) < new Date()) return false
 
-          // sinon, si le titre a le statut modification en instance
-          // l'amodiataire est encore valide (survie provisoire)
-          // ou, si le titre a le statut échu
-          // on affiche le dernier amodiataire
-          return ['mod', 'ech'].includes(titreStatutId)
+            // sinon, si le titre a le statut modification en instance
+            // l'amodiataire est encore valide (survie provisoire)
+            // ou, si le titre a le statut échu
+            // on affiche le dernier amodiataire
+            return ['mod', 'ech'].includes(titreStatutId)
+          }
+
+          return true
         }
-
-        return true
-      })
+      )
 
       // si l'étape existe, retourne son id
       // sinon retourne `null`
