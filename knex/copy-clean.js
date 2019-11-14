f2a565d3e4eddf96ca072bc9ab9bb5a3f622e929
François Romain <francoisromain@gmail.com>
2019-11-14 13:49:53 +0100

    perf(db): optimise le dump de base de données (#218)

--- knex/copy-clean.js
+++ knex/copy-clean.js
@@ -13,15 +13,15 @@ const titresIds = process.env.DUMP_TITRES_IDS.split(',')
 const run = async () => {
   try {
     console.log('Copie de la base de données…')
-    await dbManager.dropDb('camino-public')
-    await dbManager.copyDb(config.knex.connection.database, 'camino-public')
+    await dbManager.dropDb('camino_public')
+    await dbManager.copyDb(config.knex.connection.database, 'camino_public')
 
     console.log('Suppression des informations confidentielles…')
 
     const connection = {
       host: process.env.PGHOST,
       port: process.env.PGPORT,
-      database: 'camino-public',
+      database: 'camino_public',
       user: process.env.PGUSER,
       password: process.env.PGPASSWORD
     }
@@ -49,6 +49,88 @@ const run = async () => {
     console.log(
       `${utilisateursDeleted} utilisateurs supprimés de la base de données`
     )
+
+    const titresEntreprises = [
+      ...(await knex('titres_titulaires')),
+      ...(await knex('titres_amodiataires'))
+    ]
+
+    const entreprisesIdsIndex = titresEntreprises.reduce(
+      (entreprisesIdsIndex, titreEntreprise) => {
+        entreprisesIdsIndex[titreEntreprise.entrepriseId] = true
+
+        return entreprisesIdsIndex
+      },
+      {}
+    )
+
+    const entreprisesDeleted = await knex('entreprises')
+      .whereNotIn('id', Object.keys(entreprisesIdsIndex))
+      .del()
+
+    console.log(
+      `${entreprisesDeleted} entreprises supprimées de la base de données`
+    )
+
+    const titresAdministrations = [
+      ...(await knex('titres_administrations_centrales')),
+      ...(await knex('titres_administrations_locales')),
+      ...(await knex('administrations__domaines'))
+    ]
+
+    const administrationsIdsIndex = titresAdministrations.reduce(
+      (administrationsIdsIndex, titreAdministration) => {
+        administrationsIdsIndex[titreAdministration.administrationId] = true
+
+        return administrationsIdsIndex
+      },
+      {}
+    )
+
+    const administrationsDeleted = await knex('administrations')
+      .whereNotIn('id', Object.keys(administrationsIdsIndex))
+      .del()
+
+    console.log(
+      `${administrationsDeleted} administrations supprimées de la base de données`
+    )
+
+    const titresCommunes = await knex('titres_communes')
+
+    const communesIdsIndex = titresCommunes.reduce(
+      (communesIdsIndex, titreCommune) => {
+        communesIdsIndex[titreCommune.communeId] = true
+
+        return communesIdsIndex
+      },
+      {}
+    )
+
+    const communesDeleted = await knex('communes')
+      .whereNotIn('id', Object.keys(communesIdsIndex))
+      .del()
+
+    console.log(`${communesDeleted} communes supprimées de la base de données`)
+
+    const titresSubstances = await knex('titres_substances')
+
+    const substancesIdsIndex = titresSubstances.reduce(
+      (substancesIdsIndex, titreSubstance) => {
+        substancesIdsIndex[titreSubstance.substanceId] = true
+
+        return substancesIdsIndex
+      },
+      {}
+    )
+
+    const substancesDeleted = await knex('substances')
+      .whereNotIn('id', Object.keys(substancesIdsIndex))
+      .del()
+
+    console.log(
+      `${substancesDeleted} substances supprimées de la base de données`
+    )
+
     process.exit()
   } catch (e) {
     console.error(chalk.red(e))
