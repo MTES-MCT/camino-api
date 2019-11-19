77792ad729f9113b2da2787e7aa4f06fbbab582f
nicolas.petitot <nicolas.petitot@developpement-durable.gouv.fr>
2019-11-19 16:51:27 +0100

    refactor(api) : renomme `administrations centrales` en `administrations gestionnaires` et `subsidiaire` en `associée`

---
+++ src/tools/dev/json/refactor-administration-migrate.js
@@ -0,0 +1,19 @@
+const { readFileSync: read, writeFileSync: write , unlinkSync: remove} = require('fs')
+const domainesIds = ['r', 'c', 'f', 'g', 'h', 'm', 's', 'w']
+
+domainesIds.forEach(domaineId => {
+  const oldFilePath = `./sources/titres-${domaineId}-titres-administrations-centrales.json`
+  const newFilePath = `./sources/titres-${domaineId}-titres-administrations-gestionnaires.json`
+  const oldTitresAdm = JSON.parse(read(oldFilePath))
+
+  const newTitresAdm = oldTitresAdm.map( titreAdm => {
+    titreAdm.associee = titreAdm.subsidiaire
+    delete titreAdm.subsidiaire
+
+    return titreAdm
+  })
+
+  remove(oldFilePath)
+
+  write(newFilePath, JSON.stringify(newTitresAdm, null, 2))
+})
