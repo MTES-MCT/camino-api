04c6a55c268ddcc6d185e44b654793dfbf53a186
nicolas.petitot <nicolas.petitot@developpement-durable.gouv.fr>
2019-12-13 19:22:55 +0100

    fix: renommer les ids des dreal

--- src/tools/dev/database/administrations-id-rename.js
+++ src/tools/dev/database/administrations-id-rename.js
@@ -9,63 +9,54 @@ const config = require('../../../../knex/config-api')
 const knex = Knex(config.knex)
 
 async function main() {
-  const administrations = await administrationsGet()
-
+  // création d'une table de conversion {old_id:new_id}
   var conversion = {}
 
+  // array des tables de relation concernées
+  const relationTable = [
+    'administrations__domaines',
+    'restrictions__etapes_types__administrations',
+    'restrictions__types__administrations',
+    'restrictions__types__statuts__administrations',
+    'titres_administrations_gestionnaires',
+    'titres_administrations_locales',
+    'utilisateurs__administrations'
+  ]
+
+  const administrations = await administrationsGet()
+
   administrations.forEach(administration => {
+    // calcul du nouvel id d'une administration suivant son type
+    //       id              |  type_id
+    // 'dea-bfc-01'          | 'dre'      --> 'dre-bfc-01'
+    // 'prefecture-01053-01' | 'pre'      --> 'pre-01053-01'
+    // 'ope-onf-973-01'      | 'ope'      --> inchangé
     const newId = administration.id.replace(
       administration.id.substring(0, administration.id.indexOf('-')),
       administration.typeId
     )
     if (newId !== administration.id) {
+      // alimente la table de conversion
       conversion[administration.id] = newId
-      administration.id = conversion[administration.id]
 
-      // insert des administrations avec le newId
-      // Promise.resolve(Administrations.query().insert(administration))
-
-      // update des administrations avec le newId
-      Promise.resolve(Administrations.query().update(administration))
+      // patch l'id s'il a changé
+      Promise.resolve(
+        Administrations.query()
+          .findById(administration.id)
+          .patch({ id: newId })
+      )
     }
   })
 
+  // parcourt chaque nouvel id et chaque table concernée, patch le nouvel id
   Object.keys(conversion).forEach(administrationId => {
-    Promise.resolve(
-      knex('administrations__domaines')
-        .update('administration_id', conversion[administrationId])
-        .where('administration_id', '=', administrationId)
-    )
-    Promise.resolve(
-      knex('restrictions__etapes_types__administrations')
-        .update('administration_id', conversion[administrationId])
-        .where('administration_id', '=', administrationId)
-    )
-    Promise.resolve(
-      knex('restrictions__types__administrations')
-        .update('administration_id', conversion[administrationId])
-        .where('administration_id', '=', administrationId)
-    )
-    Promise.resolve(
-      knex('restrictions__types__statuts__administrations')
-        .update('administration_id', conversion[administrationId])
-        .where('administration_id', '=', administrationId)
-    )
-    Promise.resolve(
-      knex('titres_administrations_gestionnaires')
-        .update('administration_id', conversion[administrationId])
-        .where('administration_id', '=', administrationId)
-    )
-    Promise.resolve(
-      knex('titres_administrations_locales')
-        .update('administration_id', conversion[administrationId])
-        .where('administration_id', '=', administrationId)
-    )
-    Promise.resolve(
-      knex('utilisateurs__administrations')
-        .update('administration_id', conversion[administrationId])
-        .where('administration_id', '=', administrationId)
-    )
+    relationTable.forEach(table => {
+      Promise.resolve(
+        knex(table)
+          .update('administration_id', conversion[administrationId])
+          .where('administration_id', '=', administrationId)
+      )
+    })
   })
 }
 
