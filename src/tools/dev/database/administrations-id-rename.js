79eafd483082943c9884f1b2aacb175613cf1c89
nicolas.petitot <nicolas.petitot@developpement-durable.gouv.fr>
2019-12-13 18:32:12 +0100

    fix: renommer les ids des dreal

--- src/tools/dev/database/administrations-id-rename.js
+++ src/tools/dev/database/administrations-id-rename.js
@@ -1,11 +1,12 @@
 import 'dotenv/config'
 import '../../../database/index'
+import Administrations from '../../../database/models/administrations'
 
-import {
-  administrationsGet,
-  administrationsUpsert,
-  administrationsUpdate
-} from '../../../database/queries/administrations'
+import { administrationsGet } from '../../../database/queries/administrations'
+
+const Knex = require('knex')
+const config = require('../../../../knex/config-api')
+const knex = Knex(config.knex)
 
 async function main() {
   const administrations = await administrationsGet()
@@ -13,28 +14,59 @@ async function main() {
   var conversion = {}
 
   administrations.forEach(administration => {
-    conversion[administration.id] = administration.id.replace(
+    const newId = administration.id.replace(
       administration.id.substring(0, administration.id.indexOf('-')),
       administration.typeId
     )
+    if (newId !== administration.id) {
+      conversion[administration.id] = newId
+      administration.id = conversion[administration.id]
 
-    administration.id = conversion[administration.id]
+      // insert des administrations avec le newId
+      // Promise.resolve(Administrations.query().insert(administration))
 
-    Promise.resolve(Administrations.query().insert(administration))
+      // update des administrations avec le newId
+      Promise.resolve(Administrations.query().update(administration))
+    }
   })
 
-  // const res = administrations.map(administration => {
-  //   // const id = administration.id
-  //   // administration.id = id.replace(
-  //   //   id.substring(0, id.indexOf('-')),
-  //   //   administration.typeId
-  //   // )
-  //   // conversion[id] = administration.id
-
-  //   return administration
-  // })
-
-  console.log(conversion)
+  Object.keys(conversion).forEach(administrationId => {
+    Promise.resolve(
+      knex('administrations__domaines')
+        .update('administration_id', conversion[administrationId])
+        .where('administration_id', '=', administrationId)
+    )
+    Promise.resolve(
+      knex('restrictions__etapes_types__administrations')
+        .update('administration_id', conversion[administrationId])
+        .where('administration_id', '=', administrationId)
+    )
+    Promise.resolve(
+      knex('restrictions__types__administrations')
+        .update('administration_id', conversion[administrationId])
+        .where('administration_id', '=', administrationId)
+    )
+    Promise.resolve(
+      knex('restrictions__types__statuts__administrations')
+        .update('administration_id', conversion[administrationId])
+        .where('administration_id', '=', administrationId)
+    )
+    Promise.resolve(
+      knex('titres_administrations_gestionnaires')
+        .update('administration_id', conversion[administrationId])
+        .where('administration_id', '=', administrationId)
+    )
+    Promise.resolve(
+      knex('titres_administrations_locales')
+        .update('administration_id', conversion[administrationId])
+        .where('administration_id', '=', administrationId)
+    )
+    Promise.resolve(
+      knex('utilisateurs__administrations')
+        .update('administration_id', conversion[administrationId])
+        .where('administration_id', '=', administrationId)
+    )
+  })
 }
 
 main()
