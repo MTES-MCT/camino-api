5b4f510ef1fb5477485f5d5322b2d4c2a1d72fe9
nicolas.petitot <nicolas.petitot@developpement-durable.gouv.fr>
2019-12-16 17:21:14 +0100

    ajoute un champs abreviation pour les administrations

--- src/tools/dev/database/administrations-abreviation-add.js
+++ src/tools/dev/database/administrations-abreviation-add.js
@@ -1,46 +1,55 @@
 import 'dotenv/config'
 import '../../../database/index'
+import PQueue from 'p-queue'
 import Administrations from '../../../database/models/administrations'
 
 import { administrationsGet } from '../../../database/queries/administrations'
 
 async function main() {
-  const abreviations = [
-    {
-      id: 'DGALN',
-      value: 'DGALN/DEB/EARM2'
-    },
-    { id: 'DGEC', value: 'DGEC/DE/SD2/2A' },
-    { id: 'DGPR', value: 'DGPR/SRT/SDRCP/BSSS' }
-  ]
-
-  const administrations = await administrationsGet()
-
-  administrations.forEach(administration => {
-    // écrit l'abréviation "DREAL - Grand Est - Siège de Metz" pour "Direction régionale de l'environnement, de l'aménagement et du logement (DREAL) - Grand Est - Siège de Metz"
-    administration.abreviation = administration.nom
-      .substring(administration.nom.indexOf('('))
-      .replace('(', '')
-      .replace(')', '')
-
-    // écrit l'abréviation sous forme d'acronyme fourni dans l'array 'abreviations'
-    const abreviation =
-      administration.service &&
-      abreviations.find(abr => {
-        return administration.service && administration.service.includes(abr.id)
-      })
-    if (abreviation) {
-      administration.abreviation = abreviation.value
+  try {
+    const abreviations = {
+      DGALN: 'DGALN/DEB/EARM2',
+      DGEC: 'DGEC/DE/SD2/2A',
+      DGPR: 'DGPR/SRT/SDRCP/BSSS'
     }
 
-    Promise.resolve(
-      Administrations.query()
-        .findById(administration.id)
-        .patch({
-          abreviation: administration.abreviation
-        })
-    )
-  })
+    const administrations = await administrationsGet()
+
+    const administrationPatchQueue = new PQueue()
+
+    const administrationPatchQueries = administrations.map(administration => {
+      const abreviationId =
+        administration.service &&
+        Object.keys(abreviations).find(id =>
+          administration.service.includes(id)
+        )
+
+      const abreviation = abreviationId
+        ? abreviations[abreviationId]
+        : // in: Direction régionale de l'environnement, de l'aménagement et du logement (DREAL) - Grand Est - Siège de Metz
+          // out: DREAL - Grand Est - Siège de Metz
+          administration.nom
+            .substring(administration.nom.indexOf('('))
+            .replace('(', '')
+            .replace(')', '')
+
+      return () =>
+        Administrations.query()
+          .findById(administration.id)
+          .patch({ abreviation })
+    })
+
+    administrationPatchQueue.addAll(administrationPatchQueries)
+
+    await administrationPatchQueue.onIdle()
+
+    console.log('mise à jour terminée')
+
+    process.exit()
+  } catch (e) {
+    console.log(e)
+    process.exit()
+  }
 }
 
 main()
