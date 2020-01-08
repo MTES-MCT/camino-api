ebba2d827667c7714356aeeff889335c7a765867
Francois Romain <francoisromain@gmail.com>
2020-01-08 17:48:45 +0100

    refactor: corrige des fautes d'othographe

--- src/api/resolvers/_fields-build.js
+++ src/api/resolvers/_fields-build.js
@@ -2,7 +2,7 @@ import * as graphqlFields from 'graphql-fields'
 
 // in: info: objet contenant les propriétés de la requête graphQl
 // out: ast avec les champs requis par le client GraphQl
-const formatBuild = info =>
+const fieldsBuild = info =>
   graphqlFields(info, {}, { excludedFields: ['__typename'] })
 
-export default formatBuild
+export default fieldsBuild
