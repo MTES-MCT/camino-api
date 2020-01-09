17018d0e64cd5bf269fbe5d88da0d7f5a9889dc4
nicolas.petitot <nicolas.petitot@developpement-durable.gouv.fr>
2020-01-09 15:26:35 +0100

    feat(geosystemes) : alimente proj4 depuis la base

--- src/tools/geo-convert.ts
+++ src/tools/geo-convert.ts
@@ -2,16 +2,17 @@
 import * as proj4 from 'proj4'
 import { geoSystemesGet } from '../database/queries/metas'
 
-const proj4BaseSynchro = async () => {
+const init = async () => {
   const geoSystemes = await geoSystemesGet()
   proj4.defs(
-    geoSystemes.map(geosystem => {
-      return [`EPSG:${geosystem.id}`, geosystem.definitionProj4]
-    })
+    geoSystemes.map(geosystem => [
+      `EPSG:${geosystem.id}`,
+      geosystem.definitionProj4
+    ])
   )
 }
 
-proj4BaseSynchro()
+init()
 
 const geoConvert = (epsgId: string, coords: proj4.TemplateCoordinates) => {
   return proj4(`EPSG:${epsgId}`, 'EPSG:4326', coords)
