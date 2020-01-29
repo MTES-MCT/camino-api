55a44a83853a6760d40169af48815518038097e8
Adrien Risser <risseraka@gmail.com>
2020-01-29 12:56:32 +0100

    fix(activites): ne ferme pas l'activité si elle a le statut 'déposé' (#277)

--- src/business/rules/titre-activite-statut-id-find.test.js
+++ src/business/rules/titre-activite-statut-id-find.test.js
@@ -1,17 +1,33 @@
 import titreActiviteStatutIdFind from './titre-activite-statut-id-find'
 import {
-  titreActiviteDelaiDepasse,
-  titreActiviteDelaiNonDepasse
+  titreActiviteFermee,
+  titreActiviteDeposee,
+  titreActiviteAbsenteDelaiDepasse,
+  titreActiviteEnCoursDelaiNonDepasse
 } from './__mocks__/titre-activite-statut-id-find-activites'
 
 describe("statut d'une activité", () => {
-  test('une activité dont le délai est dépassé a le statut “fermé', () => {
-    expect(titreActiviteStatutIdFind(titreActiviteDelaiDepasse)).toEqual('fer')
+  test('une activité dont le statut est “fermé" garde le statut "fermé"', () => {
+    expect(titreActiviteStatutIdFind(titreActiviteFermee)).toEqual(
+      titreActiviteFermee.activiteStatutId
+    )
   })
 
-  test("une activité dont le délai n'est pas dépassé ne change pas de statut", () => {
-    expect(titreActiviteStatutIdFind(titreActiviteDelaiNonDepasse)).toEqual(
-      titreActiviteDelaiNonDepasse.activiteStatutId
+  test('une activité dont le statut est “déposé" garde le statut "déposé"', () => {
+    expect(titreActiviteStatutIdFind(titreActiviteDeposee)).toEqual(
+      titreActiviteDeposee.activiteStatutId
     )
+  })
+
+  test('une activité dont statut est "abs" et le délai est dépassé a le statut “fermé', () => {
+    expect(titreActiviteStatutIdFind(titreActiviteAbsenteDelaiDepasse)).toEqual(
+      'fer'
+    )
+  })
+
+  test('une activité dont le statut est "enc" dont le délai n\'est pas dépassé ne change pas de statut', () => {
+    expect(
+      titreActiviteStatutIdFind(titreActiviteEnCoursDelaiNonDepasse)
+    ).toEqual(titreActiviteEnCoursDelaiNonDepasse.activiteStatutId)
   })
 })
