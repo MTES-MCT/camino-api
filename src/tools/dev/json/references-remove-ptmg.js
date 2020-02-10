48ea65eb3b8d43d2316eecf821b59d0c8f867b98
nicolas.petitot <nicolas.petitot@developpement-durable.gouv.fr>
2020-02-10 17:11:08 +0100

    retire 'PTMG-' des valeurs des références des titres m

--- src/tools/dev/json/references-remove-ptmg.js
+++ src/tools/dev/json/references-remove-ptmg.js
@@ -3,12 +3,8 @@ const fs = require('fs')
 const filePath = './sources/titres-m-titres-references.json'
 const titresMReferences = JSON.parse(fs.readFileSync(filePath).toString())
 
-try {
-  titresMReferences.forEach(tmr => {
-    tmr.nom = tmr.nom.replace('PTMG-', '')
-  })
+titresMReferences.forEach(tmr => {
+  tmr.nom = tmr.nom.replace('PTMG-', '')
+})
 
-  fs.writeFileSync(filePath, JSON.stringify(titresMReferences, null, 2))
-} catch (e) {
-  console.log(e.message)
-}
+fs.writeFileSync(filePath, JSON.stringify(titresMReferences, null, 2))
