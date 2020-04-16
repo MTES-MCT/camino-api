08f42a77d17307f32f1b532ee7ec61dacbf9a7b6
Adrien Risser <risseraka@gmail.com>
2020-04-16 10:47:23 +0200

    chore: ajoute un outil de recherche d'id manquants entre deux fichiers (#364)

---
+++ dev/json/error-find-titres.js
@@ -0,0 +1,21 @@
+const domainesIds = ['c', 'f', 'g', 'h', 'm', 'r', 's', 'w']
+
+const errorFind = (a, b, join) => {
+  const elementsA = domainesIds.flatMap(domaineId =>
+    require(`../../sources/titres-${domaineId}-titres-${a}.json`)
+  )
+
+  const elementsB = domainesIds.flatMap(domaineId =>
+    require(`../../sources/titres-${domaineId}-titres-${b}.json`)
+  )
+
+  elementsB.forEach(r => {
+    const p = elementsA.find(p => p.id === r[join])
+
+    if (!p) {
+      console.log(r)
+    }
+  })
+}
+
+errorFind('etapes', 'documents', 'titre_etape_id')
