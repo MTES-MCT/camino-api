616cbd4d21e423e151e5f62d7eb277af9689195c
vmaubert <v.maubert@code-troopers.com>
2020-07-02 12:09:49 +0200

    test(entreprises): ajoute des tests sur la création et modification d'une entreprise (#461)

--- tests/_utils.ts
+++ tests/_utils.ts
@@ -2,7 +2,10 @@ import * as fs from 'fs'
 import * as path from 'path'
 import * as jwt from 'jsonwebtoken'
 
-import { IUtilisateur } from '../src/types'
+import { IPermissionId, IUtilisateur } from '../src/types'
+import * as request from 'supertest'
+import { app, knex } from './init'
+import userAdd = require('../knex/user-add')
 
 const queryImport = (nom: string) =>
   fs
@@ -13,4 +16,31 @@ const queryImport = (nom: string) =>
 const tokenCreate = (user: Partial<IUtilisateur>) =>
   jwt.sign(user, process.env.JWT_SECRET as string)
 
-export { queryImport, tokenCreate }
+const graphQLCall = async (query: string, variables: {}, token?: string) => {
+  const req = request(app).post('/').send({
+    query,
+    variables
+  })
+
+  if (token) {
+    req.set('Authorization', `Bearer ${token}`)
+  }
+
+  return req
+}
+
+const tokenUserGenerate = async (permissionId: IPermissionId) => {
+  const id = `${permissionId}-user`
+  await userAdd(knex, {
+    id,
+    prenom: 'toto',
+    nom: 'test',
+    email: `test-${permissionId}@camino.local`,
+    motDePasse: 'mot-de-passe',
+    permissionId
+  })
+
+  return tokenCreate({ id })
+}
+
+export { queryImport, tokenCreate, graphQLCall, tokenUserGenerate }
