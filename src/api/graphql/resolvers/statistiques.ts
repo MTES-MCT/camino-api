2dd269a2177e6c6a529648fa96d8362f0ab6f725
nicolas.petitot <nicolas.petitot@developpement-durable.gouv.fr>
2020-08-24 14:15:44 +0200

    feat(stats): améliore la page statistiques

--- src/api/graphql/resolvers/statistiques.ts
+++ src/api/graphql/resolvers/statistiques.ts
@@ -1,14 +1,16 @@
 import { debug } from '../../../config/index'
-import { titresGet } from '../../../database/queries/titres'
+// import { titresGet } from '../../../database/queries/titres'
 import { titresActivitesGet } from '../../../database/queries/titres-activites'
+import { matomoData } from '../../../tools/api-matomo/index'
+
 const ACTIVITE_ANNEE_DEBUT = 2018
 
 const statistiques = async () => {
   try {
-    const titres = await titresGet({}, { fields: {} }, 'super')
-    const titresTotal = titres.length
+    // const titres = await titresGet({}, { fields: {} }, 'super')
+    // const titresTotal = titres.length
 
-    const titresValide = (await titresGet({}, { fields: {} })).length
+    // const titresValide = (await titresGet({}, { fields: {} })).length
 
     const titresActivites = await titresActivitesGet({}, {}, 'super')
 
@@ -18,19 +20,19 @@ const statistiques = async () => {
         titreActivite.statutId === 'dep'
     ).length
 
-    const titresActivites2018Depose = titresActivites.filter(
-      titreActivite =>
-        titreActivite.annee === ACTIVITE_ANNEE_DEBUT &&
-        titreActivite.statutId === 'dep'
-    ).length
+    // const titresActivites2018Depose = titresActivites.filter(
+    //   titreActivite =>
+    //     titreActivite.annee === ACTIVITE_ANNEE_DEBUT &&
+    //     titreActivite.statutId === 'dep'
+    // ).length
 
-    const titreActivite2018Total = titresActivites.filter(
-      titreActivite => titreActivite.annee === ACTIVITE_ANNEE_DEBUT
-    ).length
+    // const titreActivite2018Total = titresActivites.filter(
+    //   titreActivite => titreActivite.annee === ACTIVITE_ANNEE_DEBUT
+    // ).length
 
-    const titreActivites2018Ratio = Math.round(
-      (titresActivites2018Depose / titreActivite2018Total) * 100
-    )
+    // const titreActivites2018Ratio = Math.round(
+    //   (titresActivites2018Depose / titreActivite2018Total) * 100
+    // )
 
     const titresActivitesBeneficesEntreprise = Math.round(
       (titresActivitesDepose * 2) / 7
@@ -40,13 +42,30 @@ const statistiques = async () => {
       (titresActivitesDepose * 1) / 7
     )
 
+    const {
+      nbSearchArray,
+      // nbSearchObject,
+      nbAction,
+      timeSession,
+      nbDonwload
+    } = await matomoData()
+    // console.log('nbSearchObject :>> ', nbSearchObject)
+    console.log('nbSearchArray :>> ', nbSearchArray)
+    console.log('nbAction :>> ', nbAction)
+    console.log('timeSession :>> ', timeSession)
+    console.log('nbDonwload :>> ', nbDonwload)
+
     return {
-      titresTotal,
-      titresValide,
-      titresActivitesDepose,
-      titreActivites2018Ratio,
+      // titresTotal,
+      // titresValide,
+      // titresActivitesDepose,
+      // titreActivites2018Ratio,
       titresActivitesBeneficesEntreprise,
-      titresActivitesBeneficesAdministration
+      titresActivitesBeneficesAdministration,
+      nbSearchArray,
+      nbAction,
+      timeSession,
+      nbDonwload
     }
   } catch (e) {
     if (debug) {
---
+++ src/tools/api-matomo/index.ts
@@ -0,0 +1,150 @@
+/* eslint-disable camelcase */
+import fetch from 'node-fetch'
+
+const matomoData = async () => {
+  // calcul du mois de départ
+
+  const statsNbrMonth = parseInt(process.env.STATS_NBR_MONTH!) - 1
+
+  const date = new Date()
+  date.setMonth(date.getMonth() - statsNbrMonth)
+  date.setDate(1)
+  const startDate = date.toISOString().slice(0, 10)
+
+  // Datas de la page 'Récapitulatif' des visites dans matomo
+  // url
+  const pathVisit = `${process.env
+    .API_MATOMO_URL!}?date=${startDate},today&expanded=1&filter_limit=-1&format=JSON&format_metrics=1&idSite=${process
+    .env
+    .API_MATOMO_ID!}&method=API.get&module=API&period=month&token_auth=${process
+    .env.API_MATOMO_TOKEN!}`
+
+  // Matomo retourne un objet dont les clés sont les mois, et les valeurs sont des objets dont les clés utiles aux stats sont
+  // nb_searches : nombre de recherches (int)
+  // nb_actions_per_visit : nombre moyen d'actions (int)
+  // avg_time_on_site : temps de session moyen (string)
+  // nb_downloads : nombre de téléchargements
+  const matomoVisitData = await fetch(pathVisit).then(res => res.json())
+
+  // tableau des mois
+  const months = Object.keys(matomoVisitData)
+  // mois courant
+  const currentMonth = months[months.length - 1]
+  // données du mois courant
+  const currentData = matomoVisitData[currentMonth]
+  // objet mois:nbr de recherches
+  const nbSearchObject = JSON.parse(JSON.stringify(matomoVisitData))
+  const nbSearchArray: {
+    month: string
+    value: string
+  }[] = []
+  Object.keys(nbSearchObject).forEach(key => {
+    nbSearchObject[key] = (nbSearchObject[key].nb_searches || 0).toString()
+    nbSearchArray.push({
+      month: key,
+      value: nbSearchObject[key]
+    })
+  })
+
+  // nombre d'action du mois courant
+  const nbAction = currentData.nb_actions_per_visit.toString()
+  // temps de session du mois courant
+  const timeSession = formatTime(currentData.avg_time_on_site)
+  // nombre de téléchargements du mois courant
+  const nbDonwload = currentData.nb_downloads.toString()
+
+  // Datas des évènements, catégorie 'titres-sections', actions:
+  // titre-ajouter (n'existe pas actuellement)
+  // titre-editer
+  // titre-demarche_ajouter
+  // titre-demarche_editer
+  // titre-demarche_supprimer
+  // titre-etape_ajouter
+  // titre-etape_editer
+  // titre-etape_supprimer
+  // titre-etape-doc_ajouter
+  // titre-etape-doc_editer (n'existe pas actuellement)
+  const eventActionsArray = [
+    'titre-ajouter',
+    'titre-editer',
+    'titre-demarche_ajouter',
+    'titre-demarche_editer',
+    'titre-demarche_supprimer',
+    'titre-etape_ajouter',
+    'titre-etape_editer',
+    'titre-etape_supprimer',
+    'titre-etape-doc_ajouter',
+    'titre-etape-doc_editer'
+  ]
+
+  // calcul de la liste des dates (la requête s'effectue mois par mois)
+  const dateArray = getDateArray(statsNbrMonth)
+
+  const datasArray = await Promise.all(
+    dateArray.map(async date => {
+      // url
+      const pathSection = `${process.env.API_MATOMO_URL}?date=${date}&expanded=1&filter_limit=-1&format=JSON&idSite=${process.env.API_MATOMO_ID}&method=Events.getCategory&module=API&period=month&segment=&token_auth=${process.env.API_MATOMO_TOKEN}`
+
+      const matomoSectionData = fetch(pathSection).then(res => res.json())
+
+      return matomoSectionData
+    })
+  ).then(res => {
+    // Matomo retourne un tableau d'objets dont les clés utiles aux stats sont
+    // label : le nom de la catégorie d'évènement
+    // subtable : tableau d'objets dont les clés utiles aux stats sont
+    //   |->   label : le nom de l'action de l'évènement
+    //   |->   nb_events : le nombre d'action de l'évènement
+
+    return res.map(data => {
+      if (!data || !data.length) {
+        return 0
+      }
+      const nbMaj = data
+        .find((cat: { label: string }) => cat.label == 'titre-sections')
+        .subtable.reduce((acc: any[], eventAction: { label: string }) => {
+          if (eventActionsArray.includes(eventAction.label)) {
+            acc.push(eventAction)
+          }
+
+          return acc
+        }, [])
+        .map((eventAction: { nb_events: any }) => eventAction.nb_events)
+        .reduce((total: number, nbEvent: number) => (total += nbEvent), 0)
+
+      return nbMaj
+    })
+  })
+  console.log('datasArray :>> ', datasArray)
+  // const nbMajArray =
+
+  return {
+    nbSearchArray,
+    nbAction,
+    timeSession,
+    nbDonwload
+  }
+}
+
+const formatTime = (time: string) => {
+  // si le temps ne présente que des secondes, l'afficher ainsi
+  // sinon, ne garder que les minutes
+  const index = time.search('min')
+
+  return index == -1 ? time : time.substring(0, index + 3).replace(' ', '')
+}
+
+const getDateArray = (nbrMonth: number) => {
+  const dateArray = []
+
+  for (let i = 0; i <= nbrMonth; i++) {
+    const startDate = new Date()
+    startDate.setMonth(startDate.getMonth() - i)
+    startDate.setDate(1)
+    dateArray[nbrMonth - i] = startDate.toISOString().slice(0, 10)
+  }
+
+  return dateArray
+}
+
+export { matomoData }
