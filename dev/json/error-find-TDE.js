aa7f99a7575320e2a7e01862962e5c28b320b2ea
nicolas.petitot <nicolas.petitot@developpement-durable.gouv.fr>
2020-08-31 11:01:26 +0200

    répertorie les étapes en base dont le type n'est pas paramétré dans le tableau TDE

--- dev/json/error-find-TDE.js
+++ dev/json/error-find-TDE.js
@@ -33,43 +33,41 @@ const etapesTypes = require(`../../sources/etapes-types.json`)
 
 const tdeTypes = require(`../../sources/titres-types--demarches-types--etapes-types.json`)
 
-const etapes = titresEtapes
-  .map(te => {
-    const etape = {}
+const etapes = titresEtapes.reduce((acc, te) => {
+  const etape = {}
 
-    const demarche = titresDemarches.find(td => td.id === te.titre_demarche_id)
+  const demarche = titresDemarches.find(td => td.id === te.titre_demarche_id)
 
-    const titre = titres.find(t => t.id === demarche.titre_id)
+  const titre = titres.find(t => t.id === demarche.titre_id)
 
-    const etapeType = etapesTypes.find(et => et.id === te.type_id)
+  const etapeType = etapesTypes.find(et => et.id === te.type_id)
 
-    etape.id_complet_etape = te.id
-    etape.nom_etape = etapeType.nom
-    etape.type_titre_id = titre.type_id
-    etape.type_demarche_id = demarche.type_id
-    etape.type_etape_id = te.type_id
-    etape.statut_etape_id = te.statut_id
+  etape.id_complet_etape = te.id
+  etape.nom_etape = etapeType.nom
+  etape.type_titre_id = titre.type_id
+  etape.type_demarche_id = demarche.type_id
+  etape.type_etape_id = te.type_id
+  etape.statut_etape_id = te.statut_id
 
-    return etape
-  })
-  .reduce((acc, etape) => {
-    const tdeType = tdeTypes.find(
-      tde =>
-        tde.titre_type_id === etape.type_titre_id &&
-        tde.demarche_type_id === etape.type_demarche_id &&
-        tde.etape_type_id === etape.type_etape_id
-    )
+  const tdeType = tdeTypes.find(
+    tde =>
+      tde.titre_type_id === etape.type_titre_id &&
+      tde.demarche_type_id === etape.type_demarche_id &&
+      tde.etape_type_id === etape.type_etape_id
+  )
 
-    if (tdeType) {
-      return acc
-    }
-
-    acc.push(etape)
-
+  if (tdeType) {
     return acc
-  }, [])
+  }
+
+  acc.push(etape)
+
+  return acc
+}, [])
 
 const sheet = xlsx.utils.json_to_sheet(etapes)
 const contenu = xlsx.utils.sheet_to_csv(sheet)
 
 fs.writeFileSync(`${filePath}`, contenu)
+
+console.log(`fichier ${filePath} créé`)
