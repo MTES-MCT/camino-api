63be3f1837eec6adeb337670d30cc10572aaba91
nicolas.petitot <nicolas.petitot@developpement-durable.gouv.fr>
2020-09-18 16:06:47 +0200

    feat(flux): ajoute 'régions' 'departements' 'communes' dans les properties des geojson

--- src/api/rest/format/titres.ts
+++ src/api/rest/format/titres.ts
@@ -23,11 +23,75 @@ const titreContenuFormat = (titre: ITitre) =>
 
 const titresFormatTable = (titres: ITitre[]) =>
   titres.map(titre => {
-    const {
-      communes,
-      departements,
-      regions
-    } = titreCommunesDepartementsRegions(titre)
+    const { communes, departements, regions } = titre.pays!.reduce(
+      (
+        {
+          communes,
+          departements,
+          regions
+        }: { communes: string[]; departements: string[]; regions: string[] },
+        pay
+      ) => {
+        const {
+          payRegions,
+          payDepartements,
+          payCommunes
+        } = pay.regions!.reduce(
+          (
+            {
+              payRegions,
+              payDepartements,
+              payCommunes
+            }: {
+              payRegions: string[]
+              payDepartements: string[]
+              payCommunes: string[]
+            },
+            region
+          ) => {
+            const {
+              regionDepartements,
+              regionCommunes
+            } = region.departements!.reduce(
+              (
+                {
+                  regionDepartements,
+                  regionCommunes
+                }: { regionDepartements: string[]; regionCommunes: string[] },
+                departement
+              ) => {
+                regionDepartements.push(departement.nom)
+                regionCommunes.push(
+                  ...departement.communes!.map(
+                    commune =>
+                      `${commune.nom} (${
+                        Math.round(commune.surface! / 100) / 10000
+                      })`
+                  )
+                )
+
+                return { regionDepartements, regionCommunes }
+              },
+              { regionDepartements: [], regionCommunes: [] }
+            )
+
+            payRegions.push(region.nom)
+            payDepartements.push(...regionDepartements)
+            payCommunes.push(...regionCommunes)
+
+            return { payRegions, payDepartements, payCommunes }
+          },
+          { payRegions: [], payDepartements: [], payCommunes: [] }
+        )
+
+        regions.push(...payRegions)
+        departements.push(...payDepartements)
+        communes.push(...payCommunes)
+
+        return { communes, departements, regions }
+      },
+      { communes: [], departements: [], regions: [] }
+    )
 
     const titreReferences = titre.references
       ? titre.references.reduce((titreReferences: Index<string>, reference) => {
@@ -77,119 +141,40 @@ const titresFormatTable = (titres: ITitre[]) =>
 
 const titresFormatGeojson = (titres: ITitre[]) => ({
   type: 'FeatureCollection',
-  features: titres.map(titre => {
-    const {
-      communes,
-      departements,
-      regions
-    } = titreCommunesDepartementsRegions(titre)
-
-    return {
-      type: 'Feature',
-      geometry: titre.geojsonMultiPolygon?.geometry,
-      properties: {
-        id: titre.id,
-        nom: titre.nom,
-        type: titre.type!.type.nom,
-        nature: titre.type!.type.exploitation ? 'exploitation' : 'exploration',
-        domaine: titre.domaine!.nom,
-        date_debut: titre.dateDebut,
-        date_fin: titre.dateFin,
-        date_demande: titre.dateDemande,
-        statut: titre.statut!.nom,
-        substances: titre.substances!.map(s => s.nom).join(', ') || null,
-        surface_km2: titre.surface,
-        communes: communes.join(';'),
-        departements: departements.join(';'),
-        regions: regions.join(';'),
-        administrations_noms: titre.administrations!.map(a => a.nom),
-        titulaires_noms: titre.titulaires!.map(e => e.nom).join(', ') || null,
-        titulaires_legal:
-          titre
-            .titulaires!.map(e => e.legalEtranger || e.legalSiren)
-            .join(', ') || null,
-        amodiataires_noms:
-          titre.amodiataires!.map(e => e.nom).join(', ') || null,
-        amodiataires_legal:
-          titre
-            .amodiataires!.map(e => e.legalEtranger || e.legalSiren)
-            .join(', ') || null,
-        references:
-          titre.references &&
-          titre.references
-            .map(reference => `${reference.type!.nom}: ${reference.nom}`)
-            .join(', '),
-        ...titreContenuFormat(titre)
-      }
+  features: titres.map(titre => ({
+    type: 'Feature',
+    geometry: titre.geojsonMultiPolygon?.geometry,
+    properties: {
+      id: titre.id,
+      nom: titre.nom,
+      type: titre.type!.type.nom,
+      nature: titre.type!.type.exploitation ? 'exploitation' : 'exploration',
+      domaine: titre.domaine!.nom,
+      date_debut: titre.dateDebut,
+      date_fin: titre.dateFin,
+      date_demande: titre.dateDemande,
+      statut: titre.statut!.nom,
+      substances: titre.substances!.map(s => s.nom).join(', ') || null,
+      surface_km2: titre.surface,
+      administrations_noms: titre.administrations!.map(a => a.nom),
+      titulaires_noms: titre.titulaires!.map(e => e.nom).join(', ') || null,
+      titulaires_legal:
+        titre
+          .titulaires!.map(e => e.legalEtranger || e.legalSiren)
+          .join(', ') || null,
+      amodiataires_noms: titre.amodiataires!.map(e => e.nom).join(', ') || null,
+      amodiataires_legal:
+        titre
+          .amodiataires!.map(e => e.legalEtranger || e.legalSiren)
+          .join(', ') || null,
+      references:
+        titre.references &&
+        titre.references
+          .map(reference => `${reference.type!.nom}: ${reference.nom}`)
+          .join(', '),
+      ...titreContenuFormat(titre)
     }
-  })
+  }))
 })
-
-const titreCommunesDepartementsRegions = titre =>
-  titre.pays!.reduce(
-    (
-      {
-        communes,
-        departements,
-        regions
-      }: { communes: string[]; departements: string[]; regions: string[] },
-      pay
-    ) => {
-      const { payRegions, payDepartements, payCommunes } = pay.regions!.reduce(
-        (
-          {
-            payRegions,
-            payDepartements,
-            payCommunes
-          }: {
-            payRegions: string[]
-            payDepartements: string[]
-            payCommunes: string[]
-          },
-          region
-        ) => {
-          const {
-            regionDepartements,
-            regionCommunes
-          } = region.departements!.reduce(
-            (
-              {
-                regionDepartements,
-                regionCommunes
-              }: { regionDepartements: string[]; regionCommunes: string[] },
-              departement
-            ) => {
-              regionDepartements.push(departement.nom)
-              regionCommunes.push(
-                ...departement.communes!.map(
-                  commune =>
-                    `${commune.nom} (${
-                      Math.round(commune.surface! / 100) / 10000
-                    })`
-                )
-              )
-
-              return { regionDepartements, regionCommunes }
-            },
-            { regionDepartements: [], regionCommunes: [] }
-          )
-
-          payRegions.push(region.nom)
-          payDepartements.push(...regionDepartements)
-          payCommunes.push(...regionCommunes)
-
-          return { payRegions, payDepartements, payCommunes }
-        },
-        { payRegions: [], payDepartements: [], payCommunes: [] }
-      )
-
-      regions.push(...payRegions)
-      departements.push(...payDepartements)
-      communes.push(...payCommunes)
-
-      return { communes, departements, regions }
-    },
-    { communes: [], departements: [], regions: [] }
-  )
 
 export { titresFormatGeojson, titresFormatTable }
