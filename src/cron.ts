8a13c602b69f5e3387e07cad26983cfbeb3769aa
vmaubert <v.maubert@code-troopers.com>
2020-11-16 11:15:55 +0100

    feat(daily): envoie tous les jours le résultat du daily par email

--- src/cron.ts
+++ src/cron.ts
@@ -1,10 +1,19 @@
+import 'dotenv/config'
+import './init'
 import { job } from 'cron'
 import daily from './business/daily'
 import documentsCheck from './tools/documents/check'
+import './config/logger-cron'
+import { emailSend } from './tools/emails-send'
+import { readFileSync, unlinkSync } from 'fs'
 
 const tasks = async () => {
   await daily()
   await documentsCheck()
+
+  const emailBody = readFileSync('cron.log').toString()
+  await emailSend(process.env.ADMIN_EMAIL!, 'Résultats du daily', emailBody)
+  unlinkSync('cron.log')
 }
 
 job(
