39e2475d2d009ecb239f6b08a753efb02b1f24e8
vmaubert <v.maubert@code-troopers.com>
2021-01-13 16:09:58 +0100

    fix(cron): corrige l’exécution du daily qui était lancé une seule fois (#619)

--- src/cron.ts
+++ src/cron.ts
@@ -6,17 +6,19 @@ import documentsCheck from './tools/documents/check'
 import demarchesEtatsDefinitionsCheck from './tools/demarches-etats-definitions/check'
 import './config/logger-cron'
 import { emailSend } from './tools/emails-send'
-import { readFileSync, unlinkSync } from 'fs'
+import { readFileSync, writeFileSync } from 'fs'
 
 const tasks = async () => {
   console.info('Cron quotidien : démarrage')
+  // Réinitialise les logs qui seront envoyés par email
+  writeFileSync('cron.log', '')
+
   await daily()
   await documentsCheck()
   await demarchesEtatsDefinitionsCheck()
 
   const emailBody = readFileSync('cron.log').toString()
   await emailSend(process.env.ADMIN_EMAIL!, 'Résultats du daily', emailBody)
-  unlinkSync('cron.log')
   console.info('Cron quotidien : terminé')
 }
 
