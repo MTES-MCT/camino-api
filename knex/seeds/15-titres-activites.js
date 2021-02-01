48dbef3c4d8e2266c6cb41861691ff4b920d289e
vmaubert <v.maubert@code-troopers.com>
2021-02-01 15:46:15 +0100

    chore(reprise): supprime l’importation et le traitement des titres « reprises »

--- knex/seeds/15-titres-activites.js
+++ knex/seeds/15-titres-activites.js
@@ -2,18 +2,8 @@ const seeding = require('../seeding')
 
 const titresActivites = require('../../sources/titres-activites.json')
 
-let titresActivitesReprise
-try {
-  titresActivitesReprise = require('../../sources/titres-activites-reprise-titres-activites.json')
-} catch (e) {
-  titresActivitesReprise = []
-}
-
 const seed = seeding(async ({ insert }) => {
-  await insert(
-    'titresActivites',
-    titresActivites.concat(titresActivitesReprise)
-  )
+  await insert('titresActivites', titresActivites)
 })
 
 module.exports = seed
