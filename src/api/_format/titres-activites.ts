042c31f8b36afe24cb5b77801ec21f1f3438afb6
Francois Romain <francoisromain@gmail.com>
2021-02-02 11:23:00 +0100

    fix: converti la valeur en écriture

--- src/api/_format/titres-activites.ts
+++ src/api/_format/titres-activites.ts
@@ -1,6 +1,40 @@
-import { ITitreActivite, ITrimestre, IMois, IAnnee, IFields } from '../../types'
+import {
+  ITitreActivite,
+  ITrimestre,
+  IMois,
+  IAnnee,
+  IFields,
+  ISection,
+  IContenu
+} from '../../types'
 
 import { titreSectionsFormat } from './titres-sections'
+
+const titreActiviteContenuFormat = (
+  sections: ISection[],
+  contenu: IContenu,
+  operation: 'read' | 'write'
+) => {
+  const section = sections.find(s => s.id === 'substancesFiscales')
+
+  if (section?.elements?.length && contenu?.substancesFiscales) {
+    const substancesFiscalesIds = Object.keys(contenu?.substancesFiscales)
+
+    substancesFiscalesIds.forEach(id => {
+      const element = section!.elements!.find(e => e.id === id)
+      const ratio = element?.referenceUniteRatio
+
+      if (ratio) {
+        contenu!.substancesFiscales[id] =
+          operation === 'read'
+            ? (contenu!.substancesFiscales[id] as number) * ratio
+            : (contenu!.substancesFiscales[id] as number) / ratio
+      }
+    })
+  }
+
+  return contenu
+}
 
 const titreActiviteFormatFields = {
   periode: {},
@@ -16,20 +50,8 @@ const titreActiviteFormat = (
     ta.sections = titreSectionsFormat(ta.sections)
   }
 
-  const section = ta.sections.find(s => s.id === 'substancesFiscales')
-
-  if (section?.elements?.length && ta.contenu?.substancesFiscales) {
-    const substancesFiscalesIds = Object.keys(ta.contenu?.substancesFiscales)
-
-    substancesFiscalesIds.forEach(id => {
-      const element = section!.elements!.find(e => e.id === id)
-      const ratio = element?.referenceUniteRatio
-
-      if (ratio) {
-        ta!.contenu!.substancesFiscales[id] =
-          (ta!.contenu!.substancesFiscales[id] as number) * ratio
-      }
-    })
+  if (ta.contenu) {
+    ta.contenu = titreActiviteContenuFormat(ta.sections, ta.contenu, 'read')
   }
 
   // si
@@ -52,4 +74,8 @@ const titreActiviteFormat = (
   return ta
 }
 
-export { titreActiviteFormatFields, titreActiviteFormat }
+export {
+  titreActiviteFormatFields,
+  titreActiviteFormat,
+  titreActiviteContenuFormat
+}
