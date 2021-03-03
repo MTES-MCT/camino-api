1834c8b5e4d11786fa2b2f0b9a1a1455317d437a
vmaubert <v.maubert@code-troopers.com>
2021-03-03 11:17:38 +0100

    chore(étapes): ajoute un script qui vérifie la cohérence des statuts des étapes

---
+++ src/tools/demarches/etape-statut-check.ts
@@ -0,0 +1,40 @@
+import { titresEtapesGet } from '../../database/queries/titres-etapes'
+
+const etapeStatutCheck = async () => {
+  console.info()
+  console.info('- - -')
+  console.info('vérification des statuts des étapes en bdd')
+  console.info()
+
+  const etapes = await titresEtapesGet(
+    {},
+    {
+      fields: {
+        type: { etapesStatuts: { id: {} } },
+        demarche: { type: { id: {} } }
+      }
+    },
+    'super'
+  )
+
+  let errorsNb = 0
+
+  etapes.forEach(etape => {
+    if (!etape.type!.etapesStatuts!.map(es => es.id).includes(etape.statutId)) {
+      console.info(
+        `erreur sur le titre https://camino.beta.gouv.fr/titres/${
+          etape.demarche!.titreId
+        }, étape « ${etape.type!.nom} » a un statut inconnu`
+      )
+      // console.log(
+      //   `https://camino.beta.gouv.fr/titres/${etape.demarche!.titreId}, ${
+      //     etape.demarche!.type!.nom
+      //   }, ${etape.type!.nom}`
+      // )
+      errorsNb++
+    }
+  })
+  console.info(`erreurs : ${errorsNb} statuts inconnus`)
+}
+
+export { etapeStatutCheck }
