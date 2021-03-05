c55f5357e3c0824a8852e258e03c623b8f65cb55
nicolas.petitot <nicolas.petitot@developpement-durable.gouv.fr>
2021-03-05 12:51:54 +0100

    fix(etape): corrige l'intitulé du récépissé

---
+++ src/dev/database/etape-type-rde-section-clean.ts
@@ -0,0 +1,42 @@
+import 'dotenv/config'
+import '../../init'
+import EtapesTypes from '../../database/models/etapes-types'
+import TitresEtapes from '../../database/models/titres-etapes'
+import { IContenuElement, ISectionElement } from '../../types'
+
+const main = async () => {
+  // const etapeTypeRde = await EtapesTypes.query().where('id', 'rde').first()
+
+  // const numeroRecepisseSectionDeal = etapeTypeRde.sections
+  //   ?.find(s => s.id === 'deal')!
+  //   .elements?.find(e => e.id === 'numero-recepice') as ISectionElement
+
+  // numeroRecepisseSectionDeal.id = 'numero-recepisse'
+  // numeroRecepisseSectionDeal.nom = 'Numéro de récépissé'
+  // numeroRecepisseSectionDeal.description =
+  //   'Numéro de récépissé émis par la DEAL Service eau'
+
+  // await EtapesTypes.query().patch(etapeTypeRde).where('id', 'rde')
+
+  const titresEtapesRde = await (
+    await TitresEtapes.query().where('typeId', 'rde')
+  ).filter(te => te.contenu && te.contenu && te.contenu.deal)
+
+  titresEtapesRde.forEach(te => {
+    console.log('te :>> ', te)
+
+    const deal = te.contenu?.deal as IContenuElement
+    deal['numero-recepisse'] = deal['numero-recepice']
+    delete deal['numero-recepice']
+
+    TitresEtapes.query().patch(te).where('id', te.id)
+  })
+
+  process.exit(0)
+}
+
+main().catch(e => {
+  console.error(e)
+
+  process.exit(1)
+})
