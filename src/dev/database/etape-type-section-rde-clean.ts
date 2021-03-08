f0c077af1587345abea34fa7d43bc404048abbbf
NicoP <47322935+NeoBahamut@users.noreply.github.com>
2021-03-08 12:09:59 +0100

    fix(étape):  corrige le libellé "récépissé" dans les déclarations loi sur l'eau (#685)
    Co-authored-by: Francois Romain <francoisromain@gmail.com>

---
+++ src/dev/database/etape-type-section-rde-clean.ts
@@ -0,0 +1,50 @@
+import 'dotenv/config'
+import '../../init'
+import EtapesTypes from '../../database/models/etapes-types'
+import TitresEtapes from '../../database/models/titres-etapes'
+import { ISectionElement } from '../../types'
+
+const main = async () => {
+  const etapeTypeRde = await EtapesTypes.query().where('id', 'rde').first()
+
+  const sectionDealElementNumeroRecipisse = etapeTypeRde.sections
+    ?.find(s => s.id === 'deal')!
+    .elements?.find(e => e.id === 'numero-recepice') as ISectionElement
+
+  sectionDealElementNumeroRecipisse.id = 'numero-recepisse'
+
+  await EtapesTypes.query().patch(etapeTypeRde).where('id', 'rde')
+
+  const titresEtapesRde = await TitresEtapes.query().where('typeId', 'rde')
+
+  for (const te of titresEtapesRde) {
+    let hasChanged = false
+
+    if (te.contenu?.deal) {
+      te.contenu.deal['numero-recepisse'] = te.contenu.deal['numero-recepice']
+      delete te.contenu.deal['numero-recepice']
+      hasChanged = true
+    }
+
+    if (te.heritageContenu?.deal) {
+      te.heritageContenu.deal['numero-recepisse'] =
+        te.heritageContenu.deal['numero-recepice']
+      delete te.heritageContenu?.deal['numero-recepice']
+      hasChanged = true
+    }
+
+    if (hasChanged) {
+      console.log('étpae mise à jour', te.id)
+
+      await TitresEtapes.query().patch(te).where('id', te.id)
+    }
+  }
+
+  process.exit(0)
+}
+
+main().catch(e => {
+  console.error(e)
+
+  process.exit(1)
+})
