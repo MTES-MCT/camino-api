f3d56d4d5fbda1a8ad92d7d91879f24949f03cac
nicolas.petitot <nicolas.petitot@developpement-durable.gouv.fr>
2021-03-25 10:14:03 +0100

    améliore la lisibilité des jointures dans les queries

---
+++ src/database/queries/permissions/_test_entreprises.sql
@@ -0,0 +1,1 @@
+select "entreprises".*, exists ((select "utilisateurs".* from "utilisateurs" left join "utilisateurs__entreprises" as "u_e" on "u_e"."entreprise_id" = "entreprises"."id" and "u_e"."utilisateur_id" = '3553d2')) as "modification" from "entreprises"
