8343c743b1270047f5e6aed2535b7fd972219f09
Davy Peter BRAUN <hello@davybraun.com>
2021-09-27 11:27:28 +0200

    test(upload): déplace le mocking de tus au niveau global

--- tests/jest-setup.ts
+++ tests/jest-setup.ts
@@ -1,3 +1,5 @@
+import { Request, Response } from 'express'
+
 jest.mock('../src/tools/api-mailjet/emails', () => ({
   __esModule: true,
   emailsSend: jest.fn().mockImplementation(a => a)
@@ -9,3 +11,17 @@ jest.mock('../src/tools/api-mailjet/newsletter', () => ({
   newsletterSubscriberUpdate: jest.fn().mockImplementation(() => 'succès'),
   newsletterSubscriberCheck: jest.fn().mockImplementation(() => false)
 }))
+
+jest.mock('tus-node-server')
+
+jest.mock('../src/server/upload.ts', () => {
+  const original = jest.requireActual('../src/server/upload.ts')
+
+  return {
+    uploadAllowedMiddleware: original.uploadAllowedMiddleware,
+    graphqlUpload: original.graphqlUpload,
+    restUpload: jest.fn().mockImplementation((req: Request, res: Response) => {
+      res.sendStatus(200)
+    })
+  }
+})
