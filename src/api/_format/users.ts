bd4d4881494b3617c81d575aade8429455127352
Davy Peter BRAUN <hello@davybraun.com>
2021-10-21 11:24:52 +0200

    feat(user): rend visible dans le lien administration pour lecteur, admin

--- src/api/_format/users.ts
+++ src/api/_format/users.ts
@@ -17,7 +17,7 @@ const userFormat = (utilisateur: IUtilisateur | null) => {
 
   user.sections = {
     activites: hasPermissions,
-    administrations: permissionCheck(user?.permissionId, ['super', 'admin']),
+    administrations: permissionCheck(user?.permissionId, ['super', 'admin', 'editeur', 'lecteur']),
     utilisateurs: hasPermissions,
     metas: permissionCheck(user?.permissionId, ['super']),
     journaux: permissionCheck(user?.permissionId, ['super'])
