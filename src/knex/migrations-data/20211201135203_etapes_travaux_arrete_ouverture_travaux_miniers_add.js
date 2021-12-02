ddf503609801015206f85c1be3a67dae4820707b
Davy Peter BRAUN <hello@davybraun.com>
2021-12-02 10:43:55 +0100

    fix(statuts): complémente la migration

--- src/knex/migrations-data/20211201135203_etapes_travaux_arrete_ouverture_travaux_miniers_add.js
+++ src/knex/migrations-data/20211201135203_etapes_travaux_arrete_ouverture_travaux_miniers_add.js
@@ -1,6 +1,12 @@
 exports.up = async knex => {
+  // Ajoute l'étape "Arrêté d'ouverture de travaux miniers"
+  // à la démarche "Autorisation d'ouverture de travaux miniers".
+  const titresTypes = await knex.select().table('titres_types')
+  const etapeTypeId = 'wao'
+  const demarcheTypeId = 'aom'
+
   await knex('etapes_types').insert({
-    id: 'wao',
+    id: etapeTypeId,
     nom: "arrêté d'ouverture des travaux miniers",
     description: "Arrêté d'ouverture des travaux miniers",
     ordre: 1028,
@@ -9,11 +15,20 @@ exports.up = async knex => {
 
   await knex('etapes_types__etapes_statuts').insert([
     {
-      etapeTypeId: 'wao',
+      etapeTypeId,
       etapeStatutId: 'fai',
       ordre: 0
     }
   ])
+
+  for (const t of titresTypes) {
+    await knex('titres_types__demarches_types__etapes_types').insert({
+      titreTypeId: t.id,
+      demarcheTypeId,
+      etapeTypeId,
+      ordre: 260
+    })
+  }
 }
 
 exports.down = () => ({})
