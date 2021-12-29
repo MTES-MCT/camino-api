4c35fb310b33766136e64a192f6c61682fe48a7f
vmaubert <v.maubert@code-troopers.com>
2021-12-29 09:37:40 +0100

    build(npm): supprime une dépendance qui génère des warnings lors de l’installation de celle-ci (#862)
    * feat(cookies): utilise les cookies pour gérer l’authentification (#860)
    
    * build(npm): supprime une dépendance qui génère des warnings lors de l’installation de celle-ci

---
+++ src/@types/knex-db-manager.d.ts
@@ -0,0 +1,3 @@
+// la version 0.6.3 de types/knex-db-manager utilise une très vieille version de knex qui génère
+// des warnings lors de l’installation des dépendances NPM
+declare module 'knex-db-manager'
