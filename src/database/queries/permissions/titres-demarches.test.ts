05561d3b30caf18a519a73d87180567ecbc09c7d
vmaubert <v.maubert@code-troopers.com>
2022-03-15 14:15:42 +0100

    feat(démarches): donne la possibilité aux administrations de supprimer les démarches sans étape

--- src/database/queries/permissions/titres-demarches.test.ts
+++ src/database/queries/permissions/titres-demarches.test.ts
@@ -4,9 +4,12 @@ import Titres from '../../models/titres'
 import { idGenerate } from '../../models/_format/id-create'
 import { userSuper } from '../../user-super'
 import TitresDemarches from '../../models/titres-demarches'
-import { titresDemarchesQueryModify } from './titres-demarches'
+import {
+  titreDemarcheSuppressionSelectQuery,
+  titresDemarchesQueryModify
+} from './titres-demarches'
 import { IPermissionId } from '../../../types'
-import { titresSuppressionSelectQuery } from './titres'
+import TitresEtapes from '../../models/titres-etapes'
 
 console.info = jest.fn()
 console.error = jest.fn()
@@ -25,14 +28,14 @@ describe('titresDemarchesQueryModify', () => {
     test.each`
       permissionId    | suppression
       ${'super'}      | ${true}
-      ${'admin'}      | ${false}
-      ${'editeur'}    | ${false}
+      ${'admin'}      | ${true}
+      ${'editeur'}    | ${true}
       ${'lecteur'}    | ${false}
       ${'entreprise'} | ${false}
       ${'default'}    | ${false}
       ${undefined}    | ${false}
     `(
-      'un utilisateur $permissionId peut supprimer un titre',
+      'un utilisateur $permissionId peut supprimer une démarche qui n’a pas d’étape',
       async ({
         permissionId,
         suppression
@@ -40,7 +43,102 @@ describe('titresDemarchesQueryModify', () => {
         permissionId: IPermissionId | undefined
         suppression: boolean
       }) => {
-        expect(titresSuppressionSelectQuery(permissionId)).toBe(suppression)
+        const titreId = idGenerate()
+        await Titres.query().insert([
+          {
+            id: titreId,
+            nom: titreId,
+            statutId: 'val',
+            domaineId: 'm',
+            typeId: 'arm',
+            archive: false
+          }
+        ])
+
+        const titreDemarcheId = idGenerate()
+        await TitresDemarches.query().insert([
+          {
+            id: titreDemarcheId,
+            typeId: 'oct',
+            statutId: 'eco',
+            titreId,
+            archive: false
+          }
+        ])
+        const q = TitresDemarches.query()
+        q.select(
+          titreDemarcheSuppressionSelectQuery(
+            'titresDemarches',
+            permissionId
+          ).as('suppression')
+        )
+        const titreDemarche = await q.findById(titreDemarcheId)
+
+        expect(titreDemarche).toBeTruthy()
+        expect(titreDemarche!.suppression).toBe(suppression)
+      }
+    )
+
+    test.each`
+      permissionId    | suppression
+      ${'super'}      | ${true}
+      ${'admin'}      | ${false}
+      ${'editeur'}    | ${false}
+      ${'lecteur'}    | ${false}
+      ${'entreprise'} | ${false}
+      ${'default'}    | ${false}
+      ${undefined}    | ${false}
+    `(
+      'un utilisateur $permissionId peut supprimer une démarche qui a au moins une étape',
+      async ({
+        permissionId,
+        suppression
+      }: {
+        permissionId: IPermissionId | undefined
+        suppression: boolean
+      }) => {
+        const titreId = idGenerate()
+        await Titres.query().insert([
+          {
+            id: titreId,
+            nom: titreId,
+            statutId: 'val',
+            domaineId: 'm',
+            typeId: 'arm',
+            archive: false
+          }
+        ])
+
+        const titreDemarcheId = idGenerate()
+        await TitresDemarches.query().insert([
+          {
+            id: titreDemarcheId,
+            typeId: 'oct',
+            statutId: 'eco',
+            titreId,
+            archive: false
+          }
+        ])
+
+        await TitresEtapes.query().insert({
+          titreDemarcheId,
+          date: '2020-12-23',
+          typeId: 'mfr',
+          statutId: 'fai'
+        })
+
+        const q = TitresDemarches.query()
+        q.select(
+          titreDemarcheSuppressionSelectQuery(
+            'titresDemarches',
+            permissionId
+          ).as('suppression')
+        )
+
+        const titreDemarche = await q.findById(titreDemarcheId)
+
+        expect(titreDemarche).toBeTruthy()
+        expect(titreDemarche!.suppression).toBe(suppression)
       }
     )
   })
@@ -76,7 +174,10 @@ describe('titresDemarchesQueryModify', () => {
           archive: true
         }
       ])
-      const q = TitresDemarches.query()
+      const q = TitresDemarches.query().whereIn('titresDemarches.id', [
+        titreDemarcheId,
+        archivedTitreDemarcheId
+      ])
       titresDemarchesQueryModify(q, userSuper)
 
       const titresDemarches = await q
